b272218a81ac4b87b2067064e81068d4b4324f7f
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-06-27 14:20:27 -0400

    mixin | change how strongholds spawn

--- src/main/java/com/dilloney/speedrunnermod/mixins/misc/StrongholdFeatureStartMixin.java
+++ src/main/java/com/dilloney/speedrunnermod/mixins/misc/StrongholdFeatureStartMixin.java
@@ -1,11 +1,12 @@
 package com.dilloney.speedrunnermod.mixins.misc;
 
+import net.minecraft.structure.MarginedStructureStart;
 import net.minecraft.structure.StrongholdGenerator;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.structure.StructurePiece;
-import net.minecraft.structure.StructureStart;
-import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
@@ -19,37 +20,36 @@ import org.spongepowered.asm.mixin.Shadow;
 import java.util.List;
 
 @Mixin(StrongholdFeature.Start.class)
-public class StrongholdFeatureStartMixin extends StructureStart<DefaultFeatureConfig> {
+public abstract class StrongholdFeatureStartMixin extends MarginedStructureStart<DefaultFeatureConfig> {
 
     @Shadow @Final long seed;
 
-    public StrongholdFeatureStartMixin(StructureFeature<DefaultFeatureConfig> feature, int chunkX, int chunkZ, BlockBox box, int references, long seed) {
-        super(feature, chunkX, chunkZ, box, references, seed);
+    public StrongholdFeatureStartMixin(StructureFeature<DefaultFeatureConfig> structureFeature, ChunkPos chunkPos, int i, long l) {
+        super(structureFeature, chunkPos, i, l);
     }
 
     @Overwrite
-    public void init(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, StructureManager structureManager, int i, int j, Biome biome, DefaultFeatureConfig defaultFeatureConfig) {
+    public void init(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, StructureManager structureManager, ChunkPos chunkPos, Biome biome, DefaultFeatureConfig defaultFeatureConfig, HeightLimitView heightLimitView) {
         int var8 = 0;
 
         net.minecraft.structure.StrongholdGenerator.Start start;
         do {
-            this.children.clear();
-            this.boundingBox = BlockBox.empty();
-            this.random.setCarverSeed(this.seed + (long)(var8++), i, j);
+            this.clearChildren();
+            this.random.setCarverSeed(this.seed + (long)(var8++), chunkPos.x, chunkPos.z);
             StrongholdGenerator.init();
-            start = new net.minecraft.structure.StrongholdGenerator.Start(this.random, (i << 4) + 2, (j << 4) + 2);
-            this.children.add(start);
-            start.fillOpenings(start, this.children, this.random);
+            start = new net.minecraft.structure.StrongholdGenerator.Start(this.random, chunkPos.getOffsetX(2), chunkPos.getOffsetZ(2));
+            this.addPiece(start);
+            start.fillOpenings(start, this, this.random);
             List list = start.pieces;
 
             while(!list.isEmpty()) {
-                int l = this.random.nextInt(list.size());
-                StructurePiece structurePiece = (StructurePiece)list.remove(l);
-                structurePiece.fillOpenings(start, this.children, this.random);
+                int j = this.random.nextInt(list.size());
+                StructurePiece structurePiece = (StructurePiece)list.remove(j);
+                structurePiece.fillOpenings(start, this, this.random);
             }
 
-            this.setBoundingBoxFromChildren();
             this.randomUpwardTranslation(this.random, 32, 64);
-        } while(this.children.isEmpty() || start.portalRoom == null);
+        } while(this.hasNoChildren() || start.portalRoom == null);
+
     }
 }
