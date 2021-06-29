29cca519c2e572fe174be530b5c241cb8b55b226
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-06-29 15:08:03 -0400

    update mixins

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/misc/AreaHelperMixin.java
@@ -0,0 +1,20 @@
+package com.dilloney.speedrunnermod.mixins.misc;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.dimension.AreaHelper;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(AreaHelper.class)
+public class AreaHelperMixin {
+
+    @Shadow @Final static AbstractBlock.ContextPredicate IS_VALID_FRAME_BLOCK;
+
+    static {
+        IS_VALID_FRAME_BLOCK = (state, world, pos) -> {
+            return state.isOf(Blocks.OBSIDIAN) || state.isOf(Blocks.CRYING_OBSIDIAN);
+        };
+    }
+}
