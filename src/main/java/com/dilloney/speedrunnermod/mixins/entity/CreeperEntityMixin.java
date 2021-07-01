7cfa681aa7791d8bfa3a1945dd1a51e043009cc8
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-01 15:17:44 -0400

    fix bug where speedrunner fas' doesnt ignite creepers

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/entity/CreeperEntityMixin.java
@@ -0,0 +1,44 @@
+package com.dilloney.speedrunnermod.mixins.entity;
+
+import com.dilloney.speedrunnermod.items.ModItems;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(CreeperEntity.class)
+public abstract class CreeperEntityMixin extends HostileEntity {
+
+    protected CreeperEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow abstract void ignite();
+
+    @Overwrite
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.getItem() == Items.FLINT_AND_STEEL || itemStack.getItem() == ModItems.SPEEDRUNNER_FLINT_AND_STEEL) {
+            this.world.playSound(player, this.getX(), this.getY(), this.getZ(), SoundEvents.ITEM_FLINTANDSTEEL_USE, this.getSoundCategory(), 1.0F, this.random.nextFloat() * 0.4F + 0.8F);
+            if (!this.world.isClient) {
+                this.ignite();
+                itemStack.damage(1, player, (playerEntity) -> {
+                    playerEntity.sendToolBreakStatus(hand);
+                });
+            }
+
+            return ActionResult.success(this.world.isClient);
+        } else {
+            return super.interactMob(player, hand);
+        }
+    }
+}
