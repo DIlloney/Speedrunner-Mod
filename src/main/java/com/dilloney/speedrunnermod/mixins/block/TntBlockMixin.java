69218eac5e94bd0caa0a8f92d9d342b4180f0e7f
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-01 15:15:07 -0400

    fix bug where speedrunner fas' doesnt work on tnt blocks

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/block/TntBlockMixin.java
@@ -0,0 +1,66 @@
+package com.dilloney.speedrunnermod.mixins.block;
+
+import com.dilloney.speedrunnermod.items.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.TntBlock;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(TntBlock.class)
+public class TntBlockMixin extends Block {
+
+    public TntBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Overwrite
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (!itemStack.isOf(Items.FLINT_AND_STEEL) && !itemStack.isOf(Items.FIRE_CHARGE) && !itemStack.isOf(ModItems.SPEEDRUNNER_FLINT_AND_STEEL)) {
+            return super.onUse(state, world, pos, player, hand, hit);
+        } else {
+            primeTnt(world, pos, player);
+            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
+            Item item = itemStack.getItem();
+            if (!player.isCreative()) {
+                if (itemStack.isOf(Items.FLINT_AND_STEEL) || itemStack.isOf(ModItems.SPEEDRUNNER_FLINT_AND_STEEL)) {
+                    itemStack.damage(1, player, (playerx) -> {
+                        playerx.sendToolBreakStatus(hand);
+                    });
+                } else {
+                    itemStack.decrement(1);
+                }
+            }
+
+            player.incrementStat(Stats.USED.getOrCreateStat(item));
+            return ActionResult.success(world.isClient);
+        }
+    }
+
+    private static void primeTnt(World world, BlockPos pos, @Nullable LivingEntity igniter) {
+        if (!world.isClient) {
+            TntEntity tntEntity = new TntEntity(world, (double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, igniter);
+            world.spawnEntity(tntEntity);
+            world.playSound((PlayerEntity)null, tntEntity.getX(), tntEntity.getY(), tntEntity.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            world.emitGameEvent(igniter, GameEvent.PRIME_FUSE, pos);
+        }
+    }
+}
