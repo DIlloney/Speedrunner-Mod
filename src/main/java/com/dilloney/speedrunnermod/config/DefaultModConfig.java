fb697d0417cc3ac2d5ef700ca4de9ac4b183af09
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-04 15:44:49 -0400

    create configuration file

---
+++ src/main/java/com/dilloney/speedrunnermod/config/DefaultModConfig.java
@@ -0,0 +1,18 @@
+package com.dilloney.speedrunnermod.config;
+
+public class DefaultModConfig {
+
+    public int difficulty = 1;
+
+    public boolean makeStructuresMoreCommon = true;
+    public boolean modifiedBlockHardnessValues = true;
+    public boolean modifiedStrongholdGeneration = true;
+    public boolean modifiedNetherFortressGeneration = true;
+    public boolean modifiedLootTables = true;
+
+    public boolean killGhastUponFireball = false;
+
+    public boolean defaultParticles = true;
+    public boolean netherBiomeParticles = true;
+    public boolean modifiedLavaVision = true;
+}
