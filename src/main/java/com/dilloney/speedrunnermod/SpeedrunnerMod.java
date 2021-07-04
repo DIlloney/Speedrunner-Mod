ae905beff206e8c13ac949fa74de42f71fe64d9e
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-04 15:46:45 -0400

    update MainClass

--- src/main/java/com/dilloney/speedrunnermod/SpeedrunnerMod.java
+++ src/main/java/com/dilloney/speedrunnermod/SpeedrunnerMod.java
@@ -1,6 +1,8 @@
 package com.dilloney.speedrunnermod;
 
 import com.dilloney.speedrunnermod.blocks.ModBlocks;
+import com.dilloney.speedrunnermod.config.DefaultModConfig;
+import com.dilloney.speedrunnermod.config.ModConfigLoader;
 import com.dilloney.speedrunnermod.items.ModItems;
 import com.dilloney.speedrunnermod.registry.ModRegistry;
 import net.fabricmc.api.ModInitializer;
@@ -13,13 +15,21 @@ public class SpeedrunnerMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ModRegistry.loadConfig();
         ModRegistry.registerItems();
         ModRegistry.registerBlocks();
         ModRegistry.registerBlockItems();
         ModRegistry.registerStructureConfigs();
         ModRegistry.registerConfiguredFeatures();
         ModRegistry.registerUniqueItems();
+
+        System.out.println("Difficulty is set to 1 by default. 1 = easy, 2 = medium, 3 = hard, and 4 = extreme.");
+        System.out.println("Anything set higher or lower than any of these numbers will act as if it is set to easy difficulty.");
+        System.out.println("A restart is required after modifying the config in order for changes to take effect.");
+        System.out.println("If your config somehow breaks, just delete the file and restart your game.");
     }
+
+    public static DefaultModConfig CONFIG = ModConfigLoader.get();
 
     public static final ItemGroup SPEEDRUNNER_MOD_ITEM_GROUP = FabricItemGroupBuilder.create(
         new Identifier("speedrunnermod", "speedrunner_mod_item_group"))
