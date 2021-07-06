e9489b0b7f5d613ba399314d18eea1770d79dc6a
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-06 09:48:58 -0400

    make ModItems final

--- src/main/java/com/dilloney/speedrunnermod/items/ModItems.java
+++ src/main/java/com/dilloney/speedrunnermod/items/ModItems.java
@@ -42,6 +42,18 @@ public final class ModItems {
     public static final ToolItem SPEEDRUNNER_HOE = new ToolBases.HoeBase(ModToolMaterials.SPEEDRUNNER_SHOVEL_AXE_HOE, 0, -0.5F,
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
 
+    public static final Item SPEEDRUNNER_HELMET = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.HEAD,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
+
+    public static final Item SPEEDRUNNER_CHESTPLATE = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.CHEST,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
+
+    public static final Item SPEEDRUNNER_LEGGINGS = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.LEGS,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
+
+    public static final Item SPEEDRUNNER_BOOTS = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.FEET,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
+
     public static final BowItem SPEEDRUNNER_BOW = new SpeedrunnerBowItem(
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON).maxCount(1).maxDamage(768));
 
@@ -50,66 +62,6 @@ public final class ModItems {
 
     public static final FlintAndSteelItem SPEEDRUNNER_FLINT_AND_STEEL = new FlintAndSteelItem(
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON).maxCount(1).maxDamage(128));
-
-    public static final ToolItem ANDESITE_SWORD = new SwordItem(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 4, -2.4F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
-
-    public static final ToolItem ANDESITE_SHOVEL = new ShovelItem(ModToolMaterials.ANDESITE_SHOVEL, 2, -3.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem ANDESITE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 2, -2.8F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem ANDESITE_AXE = new ToolBases.AxeBase(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 8, -3.2F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem ANDESITE_HOE = new ToolBases.HoeBase(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 0, -2.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem GRANITE_SWORD = new SwordItem(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 4, -2.4F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem GRANITE_SHOVEL = new ShovelItem(ModToolMaterials.GRANITE_SHOVEL, 2, -3.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem GRANITE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 2, -2.8F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem GRANITE_AXE = new ToolBases.AxeBase(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 8, -3.2F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem GRANITE_HOE = new ToolBases.HoeBase(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 0, -2.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem DIORITE_SWORD = new SwordItem(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 4, -2.4F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
-
-    public static final ToolItem DIORITE_SHOVEL = new ShovelItem(ModToolMaterials.DIORITE_SHOVEL, 2, -3.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem DIORITE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 2, -2.8F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem DIORITE_AXE = new ToolBases.AxeBase(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 8, -3.2F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem DIORITE_HOE = new ToolBases.HoeBase(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 0, -2.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem BLACKSTONE_SWORD = new SwordItem(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 4, -2.4F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
-
-    public static final ToolItem BLACKSTONE_SHOVEL = new ShovelItem(ModToolMaterials.BLACKSTONE_SHOVEL, 2, -3.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem BLACKSTONE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 2, -2.8F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem BLACKSTONE_AXE = new ToolBases.AxeBase(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 8, -3.2F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
-
-    public static final ToolItem BLACKSTONE_HOE = new ToolBases.HoeBase(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 0, -2.0F,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
 
     public static final Item COOKED_PIGLIN_PORK = new Item(
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.FOOD).rarity(Rarity.COMMON).food(new FoodComponent.Builder()
@@ -233,15 +185,63 @@ public final class ModItems {
                     .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 1), 1.0F)
                     .build()));
 
-    public static final Item SPEEDRUNNER_HELMET = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.HEAD,
+    public static final ToolItem ANDESITE_SWORD = new SwordItem(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 4, -2.4F,
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
 
-    public static final Item SPEEDRUNNER_CHESTPLATE = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.CHEST,
+    public static final ToolItem ANDESITE_SHOVEL = new ShovelItem(ModToolMaterials.ANDESITE_SHOVEL, 2, -3.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem ANDESITE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 2, -2.8F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem ANDESITE_AXE = new ToolBases.AxeBase(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 8, -3.2F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem ANDESITE_HOE = new ToolBases.HoeBase(ModToolMaterials.ANDESITE_EXCLUDING_SHOVEL, 0, -2.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem GRANITE_SWORD = new SwordItem(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 4, -2.4F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem GRANITE_SHOVEL = new ShovelItem(ModToolMaterials.GRANITE_SHOVEL, 2, -3.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem GRANITE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 2, -2.8F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem GRANITE_AXE = new ToolBases.AxeBase(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 8, -3.2F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem GRANITE_HOE = new ToolBases.HoeBase(ModToolMaterials.GRANITE_EXCLUDING_SHOVEL, 0, -2.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem DIORITE_SWORD = new SwordItem(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 4, -2.4F,
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
 
-    public static final Item SPEEDRUNNER_LEGGINGS = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.LEGS,
+    public static final ToolItem DIORITE_SHOVEL = new ShovelItem(ModToolMaterials.DIORITE_SHOVEL, 2, -3.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem DIORITE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 2, -2.8F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem DIORITE_AXE = new ToolBases.AxeBase(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 8, -3.2F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem DIORITE_HOE = new ToolBases.HoeBase(ModToolMaterials.DIORITE_EXCLUDING_SHOVEL, 0, -2.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem BLACKSTONE_SWORD = new SwordItem(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 4, -2.4F,
             new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
 
-    public static final Item SPEEDRUNNER_BOOTS = new ArmorItem(ModArmorMaterials.SPEEDRUNNER, EquipmentSlot.FEET,
-            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.COMBAT).rarity(Rarity.COMMON));
+    public static final ToolItem BLACKSTONE_SHOVEL = new ShovelItem(ModToolMaterials.BLACKSTONE_SHOVEL, 2, -3.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem BLACKSTONE_PICKAXE = new ToolBases.PickaxeBase(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 2, -2.8F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem BLACKSTONE_AXE = new ToolBases.AxeBase(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 8, -3.2F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+
+    public static final ToolItem BLACKSTONE_HOE = new ToolBases.HoeBase(ModToolMaterials.BLACKSTONE_EXCLUDING_SHOVEL, 0, -2.0F,
+            new Item.Settings().group(SpeedrunnerMod.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
 }
