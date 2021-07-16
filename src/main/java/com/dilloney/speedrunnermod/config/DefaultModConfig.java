686a2f531e6b094b7548e74c22fa4794c0d42671
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-16 19:19:43 -0400

    update/tweak DefaultModConfig

--- src/main/java/com/dilloney/speedrunnermod/config/DefaultModConfig.java
+++ src/main/java/com/dilloney/speedrunnermod/config/DefaultModConfig.java
@@ -5,14 +5,15 @@ public class DefaultModConfig {
     public int difficulty = 1;
 
     public boolean makeStructuresMoreCommon = true;
+    public boolean modifiedWorldGeneration = true;
+    public boolean combineFortressAndBastion = false;
     public boolean modifiedBlockHardnessValues = true;
-    public boolean modifiedStrongholdGeneration = true;
-    public boolean modifiedNetherFortressGeneration = true;
     public boolean modifiedLootTables = true;
 
-    public boolean killGhastUponFireball = false;
-
+    public boolean disableFog = false;
     public boolean defaultParticles = true;
     public boolean netherBiomeParticles = true;
-    public boolean modifiedLavaVision = true;
+
+    public boolean killGhastUponFireball = false;
+    public boolean enableChallengeMode = false;
 }
