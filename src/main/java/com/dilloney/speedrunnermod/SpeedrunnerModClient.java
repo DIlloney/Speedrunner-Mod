7ea26d17f67335459644f6b0c830ce433dcb54f2
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-16 19:28:35 -0400

    update client class

--- src/main/java/com/dilloney/speedrunnermod/SpeedrunnerModClient.java
+++ src/main/java/com/dilloney/speedrunnermod/SpeedrunnerModClient.java
@@ -1,41 +1,18 @@
 package com.dilloney.speedrunnermod;
 
-import com.dilloney.speedrunnermod.client.BrightnessFeature;
-import com.dilloney.speedrunnermod.items.ModItems;
+import com.dilloney.speedrunnermod.registry.ModRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
-import net.minecraft.util.Identifier;
-
-import static com.dilloney.speedrunnermod.client.BrightnessFeature.*;
 
 @Environment(EnvType.CLIENT)
 public class SpeedrunnerModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        KeyBindingHelper.registerKeyBinding(BRIGHTEN_BIND);
-        KeyBindingHelper.registerKeyBinding(RAISE_BIND);
-        KeyBindingHelper.registerKeyBinding(LOWER_BIND);
-        ClientTickEvents.END_CLIENT_TICK.register(BrightnessFeature::onEndTick);
+        ModRegistry.ClientRegistry.registerFabricModelPredicateProviderRegistries();
+        ModRegistry.ClientRegistry.registerBrightnessFeatureControls();
 
-        FabricModelPredicateProviderRegistry.register(ModItems.SPEEDRUNNER_BOW.asItem(), new Identifier("pull"),
-                (itemStack, clientWorld, livingEntity, item) -> {
-                    if (livingEntity == null) {
-                        return 0.0F;
-                    } else {
-                        return livingEntity.getActiveItem() != itemStack ? 0.0F : (float) (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / 20.0F;
-                    }
-        });
-
-        FabricModelPredicateProviderRegistry.register(ModItems.SPEEDRUNNER_BOW.asItem(), new Identifier("pulling"),
-                (itemStack, clientWorld, livingEntity, item) -> {
-                    return livingEntity != null && livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack ? 1.0F : 0.0F;
-        });
-
-        System.out.println("Speedrunner Mod loaded successfully! version = 1.15 | mcversion = 1.17.1");
+        System.out.println("Speedrunner Mod loaded successfully! modVersion = 1.16 | minecraftVersion = 1.17x");
     }
 }
