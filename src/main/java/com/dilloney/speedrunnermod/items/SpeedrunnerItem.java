7cd3dcd0fc06148af56fc5e88aece0f03434fc00
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-19 22:27:09 -0400

    add crossbow, fishing rod and more

--- src/main/java/com/dilloney/speedrunnermod/items/SpeedrunnerItem.java
+++ src/main/java/com/dilloney/speedrunnermod/items/SpeedrunnerItem.java
@@ -3,8 +3,10 @@ package com.dilloney.speedrunnermod.items;
 import com.dilloney.speedrunnermod.materials.ModToolMaterials;
 import com.dilloney.speedrunnermod.registry.ModItemGroup;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
@@ -12,6 +14,7 @@ import net.minecraft.item.*;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Rarity;
 import net.minecraft.world.World;
@@ -27,18 +30,6 @@ public final class SpeedrunnerItem extends Item {
     @Override
     public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
         return 1.5F;
-    }
-
-    public static final class SpeedrunnerFlintAndSteel extends FlintAndSteelItem {
-
-        public SpeedrunnerFlintAndSteel(Settings settings) {
-            super(settings.group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON).maxCount(1).maxDamage(128));
-        }
-
-        @Override
-        public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
-            return 1.5F;
-        }
     }
 
     public static final class SpeedrunnerBow extends BowItem {
@@ -128,6 +119,7 @@ public final class SpeedrunnerItem extends Item {
             return 1.5F;
         }
 
+
         public float getBaseDamage() {
             return 3.0F;
         }
@@ -167,6 +159,79 @@ public final class SpeedrunnerItem extends Item {
         public static int getPullTime(ItemStack stack) {
             int i = EnchantmentHelper.getLevel(Enchantments.QUICK_CHARGE, stack);
             return i == 0 ? 20 : 20 - 5 * i;
+        }
+    }
+
+    public static final class SpeedrunnerFlintAndSteel extends FlintAndSteelItem {
+
+        public SpeedrunnerFlintAndSteel(Settings settings) {
+            super(settings.group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON).maxCount(1).maxDamage(128));
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+            return 1.5F;
+        }
+    }
+
+    public static final class SpeedrunnerShears extends ShearsItem {
+
+        public SpeedrunnerShears(Settings settings) {
+            super(settings.group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON).maxCount(1).maxDamage(476));
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+            if (!state.isOf(Blocks.COBWEB) && !state.isIn(BlockTags.LEAVES)) {
+                if (state.isIn(BlockTags.WOOL)) {
+                    return 7.5F;
+                } else {
+                    return !state.isOf(Blocks.VINE) && !state.isOf(Blocks.GLOW_LICHEN) ? super.getMiningSpeedMultiplier(stack, state) : 2.0F;
+                }
+            } else {
+                return 17.0F;
+            }
+        }
+    }
+
+    public static final class SpeedrunnerFishingRod extends FishingRodItem {
+
+        public SpeedrunnerFishingRod(Settings settings) {
+            super(settings.group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.TOOLS).rarity(Rarity.COMMON).maxCount(1).maxDamage(128));
+        }
+
+        @Override
+        public int getEnchantability() {
+            return 17;
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+            return 1.5F;
+        }
+    }
+
+    public static final class SpeedrunnerCarrotOnAStick extends OnAStickItem {
+
+        public SpeedrunnerCarrotOnAStick(Settings settings) {
+            super(settings.group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.MISC).rarity(Rarity.COMMON).maxDamage(50), EntityType.PIG, 3);
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+            return 1.5F;
+        }
+    }
+
+    public static final class SpeedrunnerWarpedFungusOnAStick extends OnAStickItem {
+
+        public SpeedrunnerWarpedFungusOnAStick(Settings settings) {
+            super(settings.group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.MISC).rarity(Rarity.COMMON).maxDamage(150), EntityType.STRIDER, 1);
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+            return 1.5F;
         }
     }
 }
