1b3a3a87d8679955e3b78f0ed56ef8cdc3b3ae15
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-19 22:31:32 -0400

    update entity mixins

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/entity/AbstractSkeletonEntityMixin.java
@@ -0,0 +1,76 @@
+package com.dilloney.speedrunnermod.mixins.entity;
+
+import com.dilloney.speedrunnermod.SpeedrunnerMod;
+import com.dilloney.speedrunnermod.items.ModItems;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ai.goal.BowAttackGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractSkeletonEntity.class)
+public abstract class AbstractSkeletonEntityMixin extends HostileEntity {
+
+    protected AbstractSkeletonEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow @Final BowAttackGoal<AbstractSkeletonEntity> bowAttackGoal;
+    @Shadow @Final MeleeAttackGoal meleeAttackGoal;
+
+    @Overwrite
+    public static DefaultAttributeContainer.Builder createAbstractSkeletonAttributes() {
+        if (SpeedrunnerMod.CONFIG.enableChallengeMode) {
+            return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.30D);
+        } else {
+            return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D);
+        }
+    }
+
+    @Inject(method = "initEquipment", at = @At("HEAD"))
+    private void initEquipmentMod(LocalDifficulty difficulty, CallbackInfo callbackInfo) {
+        if (SpeedrunnerMod.CONFIG.enableChallengeMode) {
+            this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.BOW));
+            this.equipStack(EquipmentSlot.CHEST, new ItemStack(ModItems.SPEEDRUNNER_CHESTPLATE));
+            this.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.DIAMOND_LEGGINGS));
+        }
+    }
+
+    @Overwrite
+    public void updateAttackType() {
+        if (this.world != null && !this.world.isClient) {
+            this.goalSelector.remove(this.meleeAttackGoal);
+            this.goalSelector.remove(this.bowAttackGoal);
+            ItemStack itemStack = this.getStackInHand(ProjectileUtil.getHandPossiblyHolding(this, Items.BOW));
+            if (itemStack.isOf(Items.BOW)) {
+                int i = 20;
+                if (SpeedrunnerMod.CONFIG.enableChallengeMode) {
+                    i = 5;
+                } else if (this.world.getDifficulty() != Difficulty.HARD) {
+                    i = 40;
+                }
+
+                this.bowAttackGoal.setAttackInterval(i);
+                this.goalSelector.add(4, this.bowAttackGoal);
+            } else {
+                this.goalSelector.add(4, this.meleeAttackGoal);
+            }
+        }
+    }
+}
