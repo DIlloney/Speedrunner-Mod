0be5e52f3080ae9cf54cafe8f362511573138051
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-23 17:24:01 -0400

    remove brightness controls and finalize BrightnessFeature

--- src/main/java/com/dilloney/speedrunnermod/client/BrightnessFeature.java
+++ src/main/java/com/dilloney/speedrunnermod/client/BrightnessFeature.java
@@ -3,14 +3,6 @@ package com.dilloney.speedrunnermod.client;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.math.MathHelper;
-import org.lwjgl.glfw.GLFW;
-
-import static java.lang.Float.POSITIVE_INFINITY;
-import static net.minecraft.util.Formatting.GREEN;
 
 @Environment(EnvType.CLIENT)
 public class BrightnessFeature {
@@ -18,52 +10,4 @@ public class BrightnessFeature {
     public static MinecraftClient client;
     public static double minBrightness = 1.0;
     public static double maxBrightness = 5.0;
-    public static double prevGamma = POSITIVE_INFINITY;
-    public static double step = 0.5;
-
-    public static final KeyBinding BRIGHTEN_BIND = new KeyBinding(
-            "key.speedrunnermod.brighten",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_V,
-            "speedrunnermod.options"
-    );
-
-    public static final KeyBinding RAISE_BIND = new KeyBinding(
-            "key.speedrunnermod.raise",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_EQUAL,
-            "speedrunnermod.options"
-    );
-
-    public static final KeyBinding LOWER_BIND = new KeyBinding(
-            "key.speedrunnermod.lower",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_MINUS,
-            "speedrunnermod.options"
-    );
-
-    public static void onEndTick(MinecraftClient client) {
-        boolean show = false;
-        while (BRIGHTEN_BIND.wasPressed()) {
-            double temp = client.options.gamma;
-            client.options.gamma = MathHelper.clamp(prevGamma, minBrightness, maxBrightness);
-            prevGamma = temp;
-            show = true;
-        }
-        double gamma = client.options.gamma;
-        while (RAISE_BIND.wasPressed()) {
-            gamma += step;
-        }
-        while (LOWER_BIND.wasPressed()) {
-            gamma -= step;
-        }
-        gamma = MathHelper.clamp(gamma, minBrightness, maxBrightness);
-        if (client.options.gamma != gamma) {
-            client.options.gamma = gamma;
-            show = true;
-        }
-        if (show) {
-            client.inGameHud.setOverlayMessage(new TranslatableText("overlay.speedrunnermod.set").append(String.format(" %d%%", Math.round(gamma * 100))).styled(s -> s.withColor(GREEN)), false);
-        }
-    }
 }
