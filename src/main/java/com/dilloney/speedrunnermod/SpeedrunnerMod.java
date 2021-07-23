456431a9bbc25327cda63df3bf8fee91beddac96
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-07-23 17:28:21 -0400

    update SpeedrunnerMod main environment

--- src/main/java/com/dilloney/speedrunnermod/SpeedrunnerMod.java
+++ src/main/java/com/dilloney/speedrunnermod/SpeedrunnerMod.java
@@ -1,11 +1,16 @@
 package com.dilloney.speedrunnermod;
 
-import com.dilloney.speedrunnermod.config.DefaultModConfig;
-import com.dilloney.speedrunnermod.config.ModConfigManager;
+import com.dilloney.speedrunnermod.config.ConfigurationOptions;
+import com.dilloney.speedrunnermod.config.ConfigFileManager;
 import com.dilloney.speedrunnermod.registry.ModRegistry;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SpeedrunnerMod implements ModInitializer {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static ConfigurationOptions CONFIG = ConfigFileManager.get();
 
     @Override
     public void onInitialize() {
@@ -17,12 +22,6 @@ public class SpeedrunnerMod implements ModInitializer {
         ModRegistry.registerModDifficulty();
         ModRegistry.registerConfiguredFeatures();
         ModRegistry.registerMisc();
-
-        System.out.println("Difficulty is set to 1 by default. 1 = easy, 2 = medium, 3 = hard, and 4 = extreme.");
-        System.out.println("Anything set higher or lower than any of these numbers will act as if it is set to easy difficulty.");
-        System.out.println("A restart is required after modifying the config in order for changes to take effect.");
-        System.out.println("If your config somehow breaks, just delete the file and restart your game.");
+        LOGGER.info("Speedrunner Mod loaded successfully! modVersion = 1.16.4 | minecraftVersion = 1.17x");
     }
-
-    public static DefaultModConfig CONFIG = ModConfigManager.get();
 }
