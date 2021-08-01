3785a82d8f63250c451aeec995c6ccf305b655d0
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-01 11:19:48 -0400

    update entity mixins

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/entity/ItemEntityMixin.java
@@ -0,0 +1,33 @@
+package com.dilloney.speedrunnermod.mixins.entity;
+
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin {
+
+    @Inject(method = "isFireImmune", at = @At("RETURN"))
+    public boolean isFireImmune(CallbackInfoReturnable info) {
+        ItemEntity item = (ItemEntity) (Object) this;
+        ItemStack stack = item.getStack();
+
+        if (stack.getItem() == Items.BLAZE_ROD) {
+            return true;
+        }
+        if (stack.getItem() == Items.BLAZE_POWDER) {
+            return true;
+        }
+        if (stack.getItem() == Items.FIRE_CHARGE) {
+            return true;
+        }
+        if (stack.getItem() == Items.ENCHANTED_GOLDEN_APPLE) {
+            return true;
+        }
+        return info.getReturnValueZ();
+    }
+}
