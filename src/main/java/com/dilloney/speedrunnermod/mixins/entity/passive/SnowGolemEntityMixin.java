39e863e46a85fe2029c11d75fb058c1b907f3c5e
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-01 11:18:35 -0400

    update entity/passive mixins

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/entity/passive/SnowGolemEntityMixin.java
@@ -0,0 +1,18 @@
+package com.dilloney.speedrunnermod.mixins.entity.passive;
+
+import com.dilloney.speedrunnermod.util.ModTags;
+import net.minecraft.entity.passive.SnowGolemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(SnowGolemEntity.class)
+public class SnowGolemEntityMixin {
+
+    @Redirect(method = "interactMob", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+    private boolean interactMob(ItemStack stack, Item isOfItem) {
+        return stack.isIn(ModTags.SHEARS);
+    }
+}
