8fff93b7ceee17be0f5093cce0a3c1c9655e39fd
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-01 11:27:49 -0400

    update client main environment

--- src/main/java/com/dilloney/speedrunnermod/SpeedrunnerModClient.java
+++ src/main/java/com/dilloney/speedrunnermod/SpeedrunnerModClient.java
@@ -4,12 +4,17 @@ import com.dilloney.speedrunnermod.registry.ModRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 
 @Environment(EnvType.CLIENT)
 public class SpeedrunnerModClient implements ClientModInitializer {
 
+    public static MinecraftClient client;
+    public static double minBrightness = 1.0;
+    public static double maxBrightness = 5.0;
+
     @Override
     public void onInitializeClient() {
-        ModRegistry.registerFabricModelPredicateProviderRegistries();
+        ModRegistry.registerFabricModelPredicateProviders();
     }
 }
