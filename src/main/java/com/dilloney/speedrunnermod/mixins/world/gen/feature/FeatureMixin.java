52318a8589d5c1930e3ced6a211603c3e086e054
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-01 11:21:19 -0400

    update world/gen/feature mixins

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/world/gen/feature/FeatureMixin.java
@@ -0,0 +1,22 @@
+package com.dilloney.speedrunnermod.mixins.world.gen.feature;
+
+import com.dilloney.speedrunnermod.SpeedrunnerMod;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.world.gen.feature.Feature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Feature.class)
+public class FeatureMixin {
+
+    @Overwrite
+    public static boolean isSoil(BlockState state) {
+        if (SpeedrunnerMod.CONFIG.modifiedWorldGeneration) {
+            return state.isIn(BlockTags.DIRT) || state.isOf(Blocks.SAND) || state.isOf(Blocks.RED_SAND);
+        } else {
+            return state.isIn(BlockTags.DIRT);
+        }
+    }
+}
---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/world/gen/feature/NetherFortressFeatureMixin.java
@@ -0,0 +1,27 @@
+package com.dilloney.speedrunnermod.mixins.world.gen.feature;
+
+import com.dilloney.speedrunnermod.SpeedrunnerMod;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.collection.Pool;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.feature.NetherFortressFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(NetherFortressFeature.class)
+public class NetherFortressFeatureMixin {
+
+    @Shadow final static Pool<SpawnSettings.SpawnEntry> MONSTER_SPAWNS;
+
+    static {
+        if (SpeedrunnerMod.CONFIG.doomMode) {
+            MONSTER_SPAWNS = Pool.of(new SpawnSettings.SpawnEntry(EntityType.BLAZE, 50, 1, 4), new SpawnSettings.SpawnEntry(EntityType.ZOMBIFIED_PIGLIN, 25, 1, 1), new SpawnSettings.SpawnEntry(EntityType.WITHER_SKELETON, 75, 4, 12), new SpawnSettings.SpawnEntry(EntityType.SKELETON, 50, 5, 5), new SpawnSettings.SpawnEntry(EntityType.MAGMA_CUBE, 20, 1, 4), new SpawnSettings.SpawnEntry(EntityType.SILVERFISH, 25, 1, 2));
+        } else if (SpeedrunnerMod.CONFIG.difficulty == 1 || SpeedrunnerMod.CONFIG.difficulty == 2) {
+            MONSTER_SPAWNS = Pool.of(new SpawnSettings.SpawnEntry(EntityType.BLAZE, 15, 1, 4), new SpawnSettings.SpawnEntry(EntityType.PIGLIN, 15, 2, 4), new SpawnSettings.SpawnEntry(EntityType.ZOMBIFIED_PIGLIN, 3, 1, 2), new SpawnSettings.SpawnEntry(EntityType.WITHER_SKELETON, 8, 1, 3), new SpawnSettings.SpawnEntry(EntityType.SKELETON, 1, 1, 3), new SpawnSettings.SpawnEntry(EntityType.MAGMA_CUBE, 1, 1, 3));
+        } else if (SpeedrunnerMod.CONFIG.difficulty == 3 || SpeedrunnerMod.CONFIG.difficulty == 4) {
+            MONSTER_SPAWNS = Pool.of(new SpawnSettings.SpawnEntry(EntityType.BLAZE, 10, 1, 3), new SpawnSettings.SpawnEntry(EntityType.PIGLIN, 5, 1, 2), new SpawnSettings.SpawnEntry(EntityType.ZOMBIFIED_PIGLIN, 8, 1, 2), new SpawnSettings.SpawnEntry(EntityType.WITHER_SKELETON, 8, 1, 3), new SpawnSettings.SpawnEntry(EntityType.SKELETON, 5, 1, 4), new SpawnSettings.SpawnEntry(EntityType.MAGMA_CUBE, 3, 1, 4));
+        } else {
+            MONSTER_SPAWNS = Pool.of(new SpawnSettings.SpawnEntry(EntityType.BLAZE, 15, 1, 4), new SpawnSettings.SpawnEntry(EntityType.PIGLIN, 15, 2, 4), new SpawnSettings.SpawnEntry(EntityType.ZOMBIFIED_PIGLIN, 3, 1, 2), new SpawnSettings.SpawnEntry(EntityType.WITHER_SKELETON, 8, 1, 3), new SpawnSettings.SpawnEntry(EntityType.SKELETON, 1, 1, 3), new SpawnSettings.SpawnEntry(EntityType.MAGMA_CUBE, 1, 1, 3));
+        }
+    }
+}
