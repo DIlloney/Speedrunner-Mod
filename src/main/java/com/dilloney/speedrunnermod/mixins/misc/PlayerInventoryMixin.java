186138bf03b8f7dcd33182698cd18f4d6273800d
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-05 15:28:13 -0400

    fix bug | misc mixins

--- src/main/java/com/dilloney/speedrunnermod/mixins/misc/PlayerInventoryMixin.java
+++ src/main/java/com/dilloney/speedrunnermod/mixins/misc/PlayerInventoryMixin.java
@@ -1,10 +1,8 @@
 package com.dilloney.speedrunnermod.mixins.misc;
 
-import java.util.Iterator;
-import java.util.List;
-
 import com.dilloney.speedrunnermod.SpeedrunnerMod;
 import com.dilloney.speedrunnermod.util.PersistanceItems;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
@@ -13,16 +11,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.Iterator;
+import java.util.List;
+
 @Mixin(PlayerInventory.class)
 public class PlayerInventoryMixin {
 
-    @Shadow @Final List<DefaultedList<ItemStack>> field_7543; PlayerInventory playerinv = (PlayerInventory)(Object)this;
+    @Shadow @Final List<DefaultedList<ItemStack>> combinedInventory; PlayerInventory playerinv = (PlayerInventory)(Object)this;
+    @Shadow @Final PlayerEntity player;
 
     @Overwrite
     public void dropAll() {
+        Iterator var1 = this.combinedInventory.iterator();
         if (SpeedrunnerMod.CONFIG.manhuntMode) {
-            Iterator var1 = this.field_7543.iterator();
-
             while(var1.hasNext()) {
                 List<ItemStack> list = (List)var1.next();
 
@@ -32,10 +33,21 @@ public class PlayerInventoryMixin {
                         if (itemStack.hasNbt() && itemStack.getNbt().contains("huntercompass") && itemStack.getNbt().getBoolean("huntercompass")) {
                             PersistanceItems newItem = new PersistanceItems(this.playerinv.player.getUuidAsString(), itemStack.copy());
                             PersistanceItems.addItems(newItem);
-                        } else {
-                            this.playerinv.player.dropItem(itemStack, true, false);
+                            this.player.dropItem(itemStack, true, false);
                             list.set(i, ItemStack.EMPTY);
                         }
+                    }
+                }
+            }
+        } else {
+            while(var1.hasNext()) {
+                List<ItemStack> list = (List)var1.next();
+
+                for(int i = 0; i < list.size(); ++i) {
+                    ItemStack itemStack = (ItemStack)list.get(i);
+                    if (!itemStack.isEmpty()) {
+                        this.player.dropItem(itemStack, true, false);
+                        list.set(i, ItemStack.EMPTY);
                     }
                 }
             }
