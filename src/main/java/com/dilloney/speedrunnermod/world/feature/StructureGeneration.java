859561bf24d1ec0eac1923519c74bffb07469b07
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-14 15:50:32 -0400

    finalize StructureGeneration

--- src/main/java/com/dilloney/speedrunnermod/world/feature/StructureGeneration.java
+++ src/main/java/com/dilloney/speedrunnermod/world/feature/StructureGeneration.java
@@ -1,6 +1,5 @@
 package com.dilloney.speedrunnermod.world.feature;
 
-import com.dilloney.speedrunnermod.SpeedrunnerMod;
 import com.dilloney.speedrunnermod.mixins.misc.StructuresConfigAccessor;
 import com.google.common.collect.ImmutableMap;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
@@ -10,36 +9,39 @@ import net.minecraft.world.gen.feature.StructureFeature;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.dilloney.speedrunnermod.SpeedrunnerMod.OPTIONS;
+
 public final class StructureGeneration {
 
-    public static void modifiedStructureGeneration() {
-        if (SpeedrunnerMod.CONFIG.makeStructuresMoreCommon) {
-            ServerWorldEvents.LOAD.register((server, world) -> {
-                Map<StructureFeature<?>, StructureConfig> map = new HashMap<>(world.getChunkManager().getChunkGenerator().getStructuresConfig().getStructures());
+    public static void registerModifiedStructureGeneration() {
+        ServerWorldEvents.LOAD.register((server, world) -> {
+            Map<StructureFeature<?>, StructureConfig> map = new HashMap<>(world.getChunkManager().getChunkGenerator().getStructuresConfig().getStructures());
 
-                map.computeIfPresent(StructureFeature.RUINED_PORTAL, (structureFeature, structureConfig) -> {
-                    return new StructureConfig(9, 8, 34222645);
-                });
-                map.computeIfPresent(StructureFeature.VILLAGE, (structureFeature, structureConfig) -> {
-                    return new StructureConfig(16, 9, 10387312);
-                });
-                map.computeIfPresent(StructureFeature.DESERT_PYRAMID, (structureFeature, structureConfig) -> {
-                    return new StructureConfig(10, 8, 14357617);
-                });
-                map.computeIfPresent(StructureFeature.SHIPWRECK, (structureFeature, structureConfig) -> {
-                    return new StructureConfig(10, 8, 165745295);
-                });
-                map.computeIfPresent(StructureFeature.FORTRESS, (structureFeature, structureConfig) -> {
-                    return new StructureConfig(8, 7, 30084232);
-                });
-                map.computeIfPresent(StructureFeature.BASTION_REMNANT, (structureFeature, structureConfig) -> {
-                    return new StructureConfig(9, 8, 30084232);
-                });
-
-                ImmutableMap<StructureFeature<?>, StructureConfig> immutableMap = ImmutableMap.copyOf(map);
-
-                ((StructuresConfigAccessor) world.getChunkManager().getChunkGenerator().getStructuresConfig()).setStructures(immutableMap);
+            map.computeIfPresent(StructureFeature.RUINED_PORTAL, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getRuinedPortalSpacing(), OPTIONS.getRuinedPortalSeparation(), 34222645);
             });
-        }
+            map.computeIfPresent(StructureFeature.VILLAGE, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getVillageSpacing(), OPTIONS.getVillageSeparation(), 10387312);
+            });
+            map.computeIfPresent(StructureFeature.DESERT_PYRAMID, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getDesertPyramidSpacing(), OPTIONS.getDesertPyramidSeparation(), 14357617);
+            });
+            map.computeIfPresent(StructureFeature.SHIPWRECK, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getShipwreckSpacing(), OPTIONS.getShipwreckSeparation(), 165745295);
+            });
+            map.computeIfPresent(StructureFeature.FORTRESS, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getFortressSpacing(), OPTIONS.getFortressSeparation(), 30084232);
+            });
+            map.computeIfPresent(StructureFeature.BASTION_REMNANT, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getBastionSpacing(), OPTIONS.getBastionSeparation(), 30084232);
+            });
+            map.computeIfPresent(StructureFeature.END_CITY, (structureFeature, structureConfig) -> {
+                return new StructureConfig(OPTIONS.getEndCitySpacing(), OPTIONS.getEndCitySeparation(), 30084232);
+            });
+
+            ImmutableMap<StructureFeature<?>, StructureConfig> immutableMap = ImmutableMap.copyOf(map);
+
+            ((StructuresConfigAccessor) world.getChunkManager().getChunkGenerator().getStructuresConfig()).setStructures(immutableMap);
+        });
     }
 }
