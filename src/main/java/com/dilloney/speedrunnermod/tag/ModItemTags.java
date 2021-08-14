e9c7e3e9c19199dc9213f4bab49a5db4a971b342
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-14 15:48:59 -0400

    add ModTags

---
+++ src/main/java/com/dilloney/speedrunnermod/tag/ModItemTags.java
@@ -0,0 +1,40 @@
+package com.dilloney.speedrunnermod.tag;
+
+import net.minecraft.item.Item;
+import net.minecraft.tag.Tag;
+
+public class ModItemTags {
+
+    public static Tag<Item> SPEEDRUNNER_ITEMS;
+    public static Tag<Item> SPEEDRUNNER_MOD_ITEMS;
+    public static Tag<Item> PIGLIN_SAFE_ARMOR;
+    public static Tag<Item> BARTERING_ITEMS;
+    public static Tag<Item> GOLDEN_ITEMS;
+    public static Tag<Item> BOWS;
+    public static Tag<Item> CROSSBOWS;
+    public static Tag<Item> SHEARS;
+    public static Tag<Item> FISHING_RODS;
+    public static Tag<Item> FLINT_AND_STEELS;
+    public static Tag<Item> SHIELDS;
+    public static Tag<Item> CARROT_ON_STICKS;
+    public static Tag<Item> WARPED_FUNGUS_ON_STICKS;
+    public static Tag<Item> FIREPROOFS;
+    public static Tag<Item> IRON_INGOTS;
+    public static Tag<Item> IRON_NUGGETS;
+    public static Tag<Item> IRON_BLOCKS;
+    public static Tag<Item> COBBLESTONES;
+    public static Tag<Item> ZEROTWO_TO_ZEROONE_HARDNESS;
+    public static Tag<Item> ZEROFIVE_TO_ZEROFOUR_HARDNESS;
+    public static Tag<Item> ZEROEIGHT_TO_ZEROSIXFIVE_HARDNESS;
+    public static Tag<Item> ZEROEIGHT_TO_ZEROSEVEN_HARDNESS;
+    public static Tag<Item> ONEFIVE_TO_ONETHREE_HARDNESS;
+    public static Tag<Item> TWOZERO_TO_ZEROEIGHT_HARDNESS;
+    public static Tag<Item> TWOZERO_TO_ONETHREE_HARDNESS;
+    public static Tag<Item> TWOZERO_TO_ONEFOUR_HARDNESS;
+    public static Tag<Item> TWOZERO_TO_ONEFIVE_HARDNESS;
+    public static Tag<Item> TWOFIVE_TO_ONEFIVE_HARDNESS;
+    public static Tag<Item> THREEZERO_TO_ONEFIVE_HARDNESS;
+    public static Tag<Item> THREEFIVE_TO_TWOZERO_HARDNESS;
+    public static Tag<Item> THREEFIVE_TO_TWOFIVE_HARDNESS;
+    public static Tag<Item> FIVEZERO_TO_THREEZERO_HARDNESS;
+}
