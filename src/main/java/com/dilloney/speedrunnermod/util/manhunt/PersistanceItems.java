b89f221ddab04f95030afe8e964fbe404680a28c
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-14 15:49:52 -0400

    update manhunt

---
+++ src/main/java/com/dilloney/speedrunnermod/util/manhunt/PersistanceItems.java
@@ -0,0 +1,32 @@
+package com.dilloney.speedrunnermod.util.manhunt;
+
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+
+public class PersistanceItems {
+    private static ArrayList<PersistanceItems> persistentPlayers = new ArrayList();
+    private String player;
+    private ItemStack itemStack;
+
+    public PersistanceItems(String player, ItemStack itemStack) {
+        this.player = player;
+        this.itemStack = itemStack;
+    }
+
+    public String getPlayerUUID() {
+        return this.player;
+    }
+
+    public ItemStack getStack() {
+        return this.itemStack;
+    }
+
+    public static void addItems(PersistanceItems player) {
+        persistentPlayers.add(player);
+    }
+
+    public static ArrayList<PersistanceItems> getItems() {
+        return persistentPlayers;
+    }
+}
