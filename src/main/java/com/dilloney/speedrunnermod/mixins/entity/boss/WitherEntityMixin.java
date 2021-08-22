727f55d02e88078055f18e950bfed78fc4d6e936
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-22 18:32:16 -0400

    Add files via upload

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/entity/boss/WitherEntityMixin.java
@@ -0,0 +1,24 @@
+package com.dilloney.speedrunnermod.mixins.entity.boss;
+
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.boss.WitherEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import static com.dilloney.speedrunnermod.SpeedrunnerMod.OPTIONS;
+
+@Mixin(WitherEntity.class)
+public class WitherEntityMixin {
+
+    /**
+     * Changes the Withers health.
+     * @author Dilloney
+     * @reason
+     */
+    @Overwrite
+    public static DefaultAttributeContainer.Builder createWitherAttributes() {
+        return OPTIONS.getModDifficulty() <= 2 ? HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 100.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.6000000238418579D).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 40.0D).add(EntityAttributes.GENERIC_ARMOR, 4.0D) : HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 300.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.6000000238418579D).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 40.0D).add(EntityAttributes.GENERIC_ARMOR, 4.0D);
+    }
+}
