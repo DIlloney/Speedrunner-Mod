9839f6ef5acfd7a7cb0b549f429eb5fbe689a598
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-08-22 18:25:33 -0400

    Add files via upload

---
+++ src/main/java/com/dilloney/speedrunnermod/mixins/client/gui/screen/option/OptionsScreenMixin.java
@@ -0,0 +1,28 @@
+package com.dilloney.speedrunnermod.mixins.client.gui.screen.option;
+
+import com.dilloney.speedrunnermod.client.ModOptionsScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OptionsScreen.class)
+public class OptionsScreenMixin extends Screen {
+
+    @Deprecated
+    protected OptionsScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void addSpeedrunnerModConfigurationScreenToOptionsScreen(CallbackInfo callbackInfo) {
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155, this.height / 6 + 144 - 6, 310, 20, new TranslatableText("speedrunnermod.options"), (button) -> {
+            this.client.openScreen(ModOptionsScreen.getConfigBuilder().build());
+        }));
+    }
+}
