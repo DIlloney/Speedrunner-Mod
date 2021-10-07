3d118113eaab5fdbb14fd1cac59ddc1e38c51fa4
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-07 16:30:26 -0400

    Update SpeedrunnerModClient.java

--- src/main/java/com/dilloney/speedrunnermod/SpeedrunnerModClient.java
+++ src/main/java/com/dilloney/speedrunnermod/SpeedrunnerModClient.java
@@ -4,17 +4,14 @@ import com.dilloney.speedrunnermod.util.ModRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 
 @Environment(EnvType.CLIENT)
 public class SpeedrunnerModClient implements ClientModInitializer {
 
-    public static MinecraftClient client;
     public static double minBrightness = 1.0;
     public static double maxBrightness = 5.0;
 
-    @Override
     public void onInitializeClient() {
-        ModRegistry.registerModels();
+        ModRegistry.loadModels();
     }
 }
