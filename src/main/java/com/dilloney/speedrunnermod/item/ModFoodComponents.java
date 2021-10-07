77af7a4da73c2dbcd234fd3dd84f871d2ebd37f0
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-07 16:26:36 -0400

    Add files via upload

---
+++ src/main/java/com/dilloney/speedrunnermod/item/ModFoodComponents.java
@@ -0,0 +1,18 @@
+package com.dilloney.speedrunnermod.item;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+
+    public static final FoodComponent SPEEDRUNNER_BULK = new FoodComponent.Builder().hunger(12).saturationModifier(1.2F).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600, 0), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 1200, 0), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 1200, 0), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 500, 1), 0.6F).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200), 0.25F).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 400), 0.25F).alwaysEdible().build();
+
+    public static final FoodComponent ROTTEN_SPEEDRUNNER_BULK = new FoodComponent.Builder().hunger(4).saturationModifier(0.1F).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 400, 0), 0.5F).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 300, 0), 0.1F).build();
+
+    public static final FoodComponent COOKED_PIGLIN_PORK = new FoodComponent.Builder().hunger(8).saturationModifier(0.8F).build();
+
+    public static final FoodComponent RAW_PIGLIN_PORK = new FoodComponent.Builder().hunger(3).saturationModifier(0.3F).build();
+
+    public static final FoodComponent COOKED_FLESH = new FoodComponent.Builder().hunger(6).saturationModifier(0.8F).build();
+}
---
+++ src/main/java/com/dilloney/speedrunnermod/item/SpeedrunnerSwordItem.java
@@ -0,0 +1,27 @@
+package com.dilloney.speedrunnermod.item;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.tag.BlockTags;
+
+public class SpeedrunnerSwordItem extends SwordItem {
+
+    public SpeedrunnerSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        if (stack.isOf(ModItems.SPEEDRUNNER_SWORD) && state.isOf(Blocks.COBWEB)) {
+            return 16.0F;
+        } else if (stack.isOf(ModItems.GOLDEN_SPEEDRUNNER_SWORD) && state.isOf(Blocks.COBWEB)) {
+            return 17.0F;
+        } else {
+            Material material = state.getMaterial();
+            return material != Material.PLANT && material != Material.REPLACEABLE_PLANT && !state.isIn(BlockTags.LEAVES) && material != Material.GOURD ? 1.0F : 1.5F;
+        }
+    }
+}
