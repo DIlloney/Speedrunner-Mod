e7f5a215c093d3f23b8d6a81b83b0c7e42747000
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-07 16:19:01 -0400

    update ModBlocks

--- src/main/java/com/dilloney/speedrunnermod/block/ModBlocks.java
+++ src/main/java/com/dilloney/speedrunnermod/block/ModBlocks.java
@@ -10,8 +10,9 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
-public final class ModBlocks {
+public class ModBlocks {
 
     public static final Block SPEEDRUNNER_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 1)
@@ -23,43 +24,37 @@ public final class ModBlocks {
             .breakByTool(FabricToolTags.PICKAXES, 1)
             .requiresTool()
             .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.STONE));
+            .sounds(BlockSoundGroup.STONE), UniformIntProvider.create(32, 64));
 
     public static final Block DEEPSLATE_SPEEDRUNNER_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 1)
             .requiresTool()
             .strength(4.5F, 4.5F)
-            .sounds(BlockSoundGroup.DEEPSLATE));
+            .sounds(BlockSoundGroup.DEEPSLATE), UniformIntProvider.create(32, 72));
 
     public static final Block NETHER_SPEEDRUNNER_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 0)
             .requiresTool()
             .strength(3.0F, 3.0F)
-            .sounds(BlockSoundGroup.NETHER_GOLD_ORE));
+            .sounds(BlockSoundGroup.NETHER_GOLD_ORE), UniformIntProvider.create(16, 36));
 
     public static final Block IGNEOUS_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 2)
             .requiresTool()
             .strength(3.0F, 3.0F)
-            .luminance((state) -> {
-                return 1;
-            }).sounds(BlockSoundGroup.STONE));
+            .sounds(BlockSoundGroup.STONE), UniformIntProvider.create(2, 6));
 
     public static final Block DEEPSLATE_IGNEOUS_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 2)
             .requiresTool()
             .strength(4.5F, 4.5F)
-            .luminance((state) -> {
-                return 1;
-            }).sounds(BlockSoundGroup.DEEPSLATE));
+            .sounds(BlockSoundGroup.DEEPSLATE), UniformIntProvider.create(2, 6));
 
     public static final Block NETHER_IGNEOUS_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 2)
             .requiresTool()
             .strength(3.0F, 3.0F)
-            .luminance((state) -> {
-                return 1;
-            }).sounds(BlockSoundGroup.NETHER_ORE));
+            .sounds(BlockSoundGroup.NETHER_ORE), UniformIntProvider.create(2, 6));
 
     public static final BlockItem SPEEDRUNNER_BLOCK_ITEM = new BlockItem(SPEEDRUNNER_BLOCK,
             new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.MATERIALS));
