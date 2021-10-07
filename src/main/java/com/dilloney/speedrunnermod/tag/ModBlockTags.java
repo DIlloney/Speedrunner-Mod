d620ce3655cb6a018dff85bc1ee79b0ea8182386
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-07 16:29:08 -0400

    Update ModBlockTags.java

--- src/main/java/com/dilloney/speedrunnermod/tag/ModBlockTags.java
+++ src/main/java/com/dilloney/speedrunnermod/tag/ModBlockTags.java
@@ -1,29 +1,28 @@
 package com.dilloney.speedrunnermod.tag;
 
-
 import net.minecraft.block.Block;
 import net.minecraft.tag.Tag;
 
 public class ModBlockTags {
 
-    public static Tag<Block> NETHER_PORTAL_BASE_BLOCKS;
-    public static Tag<Block> ZERO_ONE_HARDNESS;
-    public static Tag<Block> ZERO_TWO_HARDNESS;
-    public static Tag<Block> ZERO_THREESEVEN_HARDNESS;
-    public static Tag<Block> ZERO_FOUR_HARDNESS;
-    public static Tag<Block> ZERO_FIVE_HARDNESS;
-    public static Tag<Block> ZERO_SIX_HARDNESS;
-    public static Tag<Block> ZERO_SIXFIVE_HARDNESS;
-    public static Tag<Block> ZERO_SEVEN_HARDNESS;
-    public static Tag<Block> ZERO_EIGHT_HARDNESS;
-    public static Tag<Block> ONE_ZERO_HARDNESS;
-    public static Tag<Block> ONE_THREE_HARDNESS;
-    public static Tag<Block> ONE_FOUR_HARDNESS;
-    public static Tag<Block> ONE_FIVE_HARDNESS;
-    public static Tag<Block> ONE_SIX_HARDNESS;
-    public static Tag<Block> TWO_ZERO_HARDNESS;
-    public static Tag<Block> TWO_FIVE_HARDNESS;
-    public static Tag<Block> THREE_ZERO_HARDNESS;
-    public static Tag<Block> TEN_HARDNESS;
-    public static Tag<Block> TWENTY_FIVE_HARDNESS;
+    public static Tag.Identified<Block> ZERO_HARDNESS;
+    public static Tag.Identified<Block> ZERO_ONE_HARDNESS;
+    public static Tag.Identified<Block> ZERO_TWO_HARDNESS;
+    public static Tag.Identified<Block> ZERO_THREESEVEN_HARDNESS;
+    public static Tag.Identified<Block> ZERO_FOUR_HARDNESS;
+    public static Tag.Identified<Block> ZERO_FIVE_HARDNESS;
+    public static Tag.Identified<Block> ZERO_SIX_HARDNESS;
+    public static Tag.Identified<Block> ZERO_SIXFIVE_HARDNESS;
+    public static Tag.Identified<Block> ZERO_SEVEN_HARDNESS;
+    public static Tag.Identified<Block> ZERO_EIGHT_HARDNESS;
+    public static Tag.Identified<Block> ONE_ZERO_HARDNESS;
+    public static Tag.Identified<Block> ONE_THREE_HARDNESS;
+    public static Tag.Identified<Block> ONE_FOUR_HARDNESS;
+    public static Tag.Identified<Block> ONE_FIVE_HARDNESS;
+    public static Tag.Identified<Block> ONE_SIX_HARDNESS;
+    public static Tag.Identified<Block> TWO_ZERO_HARDNESS;
+    public static Tag.Identified<Block> TWO_FIVE_HARDNESS;
+    public static Tag.Identified<Block> THREE_ZERO_HARDNESS;
+    public static Tag.Identified<Block> TEN_HARDNESS;
+    public static Tag.Identified<Block> TWENTY_FIVE_HARDNESS;
 }
