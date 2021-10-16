66ae1e5059156c0665e491035b84790e9a14aabe
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-16 17:34:01 -0400

    Create SpeedrunnerModClient.java

---
+++ src/main/net/dilloney/speedrunnermod/SpeedrunnerModClient.java
@@ -0,0 +1,17 @@
+package net.dilloney.speedrunnermod;
+
+import net.dilloney.speedrunnermod.util.ModRegistry;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SpeedrunnerModClient implements ClientModInitializer {
+
+    public static double minBrightness = 1.0;
+    public static double maxBrightness = 5.0;
+
+    public void onInitializeClient() {
+        ModRegistry.loadModels();
+    }
+}
