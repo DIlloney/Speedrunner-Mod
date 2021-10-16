e42ef1bf0f11ac01a4dd81362061b3280730a147
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-16 17:48:10 -0400

    Create SingleRun.java

---
+++ src/main/net/dilloney/speedrunnermod/util/timer/data/SingleRun.java
@@ -0,0 +1,19 @@
+package net.dilloney.speedrunnermod.util.timer.data;
+
+public class SingleRun extends AbstractRun {
+    public long ticks;
+    public long startTimestamp;
+
+    SingleRun(long ticks) {
+        this.ticks = ticks;
+        startTimestamp = System.currentTimeMillis();
+    }
+
+    public long getGameTime() {
+        return ticks * 50;
+    }
+
+    public long getRealTimeDuration() {
+        return System.currentTimeMillis() - startTimestamp;
+    }
+}
