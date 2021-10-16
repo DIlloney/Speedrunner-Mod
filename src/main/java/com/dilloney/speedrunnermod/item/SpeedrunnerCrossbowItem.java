cc07bcd3d6117e5c3ffdecc042d3d28304385cef
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-16 17:18:14 -0400

    Update SpeedrunnerCrossbowItem.java

--- src/main/java/com/dilloney/speedrunnermod/item/SpeedrunnerCrossbowItem.java
+++ src/main/java/com/dilloney/speedrunnermod/item/SpeedrunnerCrossbowItem.java
@@ -1,25 +1,11 @@
-package com.dilloney.speedrunnermod.item;
+package net.dilloney.speedrunnermod.item;
 
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArrowItem;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
 
 public class SpeedrunnerCrossbowItem extends CrossbowItem {
-    private boolean charged = false;
-    private boolean loaded = false;
 
     public SpeedrunnerCrossbowItem(Settings settings) {
         super(settings);
@@ -33,146 +19,8 @@ public class SpeedrunnerCrossbowItem extends CrossbowItem {
         return getPullTime(stack) + 3;
     }
 
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        if (isCharged(itemStack)) {
-            shootAll(world, user, hand, itemStack, getSpeed(itemStack), 1.0F);
-            setCharged(itemStack, false);
-            return TypedActionResult.consume(itemStack);
-        } else if (!user.getArrowType(itemStack).isEmpty()) {
-            if (!isCharged(itemStack)) {
-                this.charged = false;
-                this.loaded = false;
-                user.setCurrentHand(hand);
-            }
-
-            return TypedActionResult.consume(itemStack);
-        } else {
-            return TypedActionResult.fail(itemStack);
-        }
-    }
-
-    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-        int i = this.getMaxUseTime(stack) - remainingUseTicks;
-        float f = getPullProgress(i, stack);
-        if (f >= 1.0F && !isCharged(stack) && loadProjectiles(user, stack)) {
-            setCharged(stack, true);
-            SoundCategory soundCategory = user instanceof PlayerEntity ? SoundCategory.PLAYERS : SoundCategory.HOSTILE;
-            world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ITEM_CROSSBOW_LOADING_END, soundCategory, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.5F + 1.0F) + 0.2F);
-        }
-    }
-
-    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
-        if (!world.isClient) {
-            int i = EnchantmentHelper.getLevel(Enchantments.QUICK_CHARGE, stack);
-            SoundEvent soundEvent = this.getQuickChargeSound(i);
-            SoundEvent soundEvent2 = i == 0 ? SoundEvents.ITEM_CROSSBOW_LOADING_MIDDLE : null;
-            float f = (float)(stack.getMaxUseTime() - remainingUseTicks) / (float)getPullTime(stack);
-            if (f < 0.2F) {
-                this.charged = false;
-                this.loaded = false;
-            }
-
-            if (f >= 0.2F && !this.charged) {
-                this.charged = true;
-                world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), soundEvent, SoundCategory.PLAYERS, 0.5F, 1.0F);
-            }
-
-            if (f >= 0.5F && soundEvent2 != null && !this.loaded) {
-                this.loaded = true;
-                world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), soundEvent2, SoundCategory.PLAYERS, 0.5F, 1.0F);
-            }
-        }
-    }
-
     public static int getPullTime(ItemStack stack) {
         int i = EnchantmentHelper.getLevel(Enchantments.QUICK_CHARGE, stack);
         return i == 0 ? 20 : 20 - 5 * i;
-    }
-
-    private static float getSpeed(ItemStack stack) {
-        return hasProjectile(stack, Items.FIREWORK_ROCKET) ? 1.6F : 3.15F;
-    }
-
-    private static boolean loadProjectiles(LivingEntity shooter, ItemStack projectile) {
-        int i = EnchantmentHelper.getLevel(Enchantments.MULTISHOT, projectile);
-        int j = i == 0 ? 1 : 3;
-        boolean bl = shooter instanceof PlayerEntity && ((PlayerEntity)shooter).getAbilities().creativeMode;
-        ItemStack itemStack = shooter.getArrowType(projectile);
-        ItemStack itemStack2 = itemStack.copy();
-
-        for(int k = 0; k < j; ++k) {
-            if (k > 0) {
-                itemStack = itemStack2.copy();
-            }
-
-            if (itemStack.isEmpty() && bl) {
-                itemStack = new ItemStack(Items.ARROW);
-                itemStack2 = itemStack.copy();
-            }
-
-            if (!loadProjectile(shooter, projectile, itemStack, k > 0, bl)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static boolean loadProjectile(LivingEntity shooter, ItemStack crossbow, ItemStack projectile, boolean simulated, boolean creative) {
-        if (projectile.isEmpty()) {
-            return false;
-        } else {
-            boolean bl = creative && projectile.getItem() instanceof ArrowItem;
-            ItemStack itemStack2;
-            if (!bl && !creative && !simulated) {
-                itemStack2 = projectile.split(1);
-                if (projectile.isEmpty() && shooter instanceof PlayerEntity) {
-                    ((PlayerEntity)shooter).getInventory().removeOne(projectile);
-                }
-            } else {
-                itemStack2 = projectile.copy();
-            }
-
-            putProjectile(crossbow, itemStack2);
-            return true;
-        }
-    }
-
-    private static void putProjectile(ItemStack crossbow, ItemStack projectile) {
-        NbtCompound nbtCompound = crossbow.getOrCreateTag();
-        NbtList nbtList2;
-        if (nbtCompound.contains("ChargedProjectiles", 9)) {
-            nbtList2 = nbtCompound.getList("ChargedProjectiles", 10);
-        } else {
-            nbtList2 = new NbtList();
-        }
-
-        NbtCompound nbtCompound2 = new NbtCompound();
-        projectile.writeNbt(nbtCompound2);
-        nbtList2.add(nbtCompound2);
-        nbtCompound.put("ChargedProjectiles", nbtList2);
-    }
-
-    private SoundEvent getQuickChargeSound(int stage) {
-        switch(stage) {
-            case 1:
-                return SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_1;
-            case 2:
-                return SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_2;
-            case 3:
-                return SoundEvents.ITEM_CROSSBOW_QUICK_CHARGE_3;
-            default:
-                return SoundEvents.ITEM_CROSSBOW_LOADING_START;
-        }
-    }
-
-    private static float getPullProgress(int useTicks, ItemStack stack) {
-        float f = (float)useTicks / (float)getPullTime(stack);
-        if (f > 1.0F) {
-            f = 1.0F;
-        }
-
-        return f;
     }
 }
