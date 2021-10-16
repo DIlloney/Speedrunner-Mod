796af25550ad96b8581705fb34adf24af91ce053
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-16 17:48:19 -0400

    Create Debounce.java

---
+++ src/main/net/dilloney/speedrunnermod/util/timer/Debounce.java
@@ -0,0 +1,20 @@
+package net.dilloney.speedrunnermod.util.timer;
+
+class Debounce {
+    private final long intervalMs;
+    private long lastCalled;
+
+    Debounce(long intervalMs) {
+        this.intervalMs = intervalMs;
+        lastCalled = System.currentTimeMillis();
+    }
+
+    public boolean boing() {
+        long currentTime = System.currentTimeMillis();
+        if (lastCalled + intervalMs < currentTime) {
+            lastCalled = currentTime;
+            return true;
+        }
+        return false;
+    }
+}
