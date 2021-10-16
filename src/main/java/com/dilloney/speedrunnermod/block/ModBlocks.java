429345bfa190e831d51d1153f8aed28954860660
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-16 17:15:39 -0400

    Update ModBlocks.java

--- src/main/java/com/dilloney/speedrunnermod/block/ModBlocks.java
+++ src/main/java/com/dilloney/speedrunnermod/block/ModBlocks.java
@@ -1,14 +1,10 @@
-package com.dilloney.speedrunnermod.block;
+package net.dilloney.speedrunnermod.block;
 
-import com.dilloney.speedrunnermod.item.ModItemGroup;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.OreBlock;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 
@@ -19,6 +15,12 @@ public class ModBlocks {
             .requiresTool()
             .strength(5.0F, 6.0F)
             .sounds(BlockSoundGroup.METAL));
+
+    public static final Block RAW_SPEEDRUNNER_BLOCK = new Block(FabricBlockSettings.of(Material.STONE)
+            .breakByTool(FabricToolTags.PICKAXES, 1)
+            .requiresTool()
+            .strength(5.0F, 6.0F)
+            .sounds(BlockSoundGroup.STONE));
 
     public static final Block SPEEDRUNNER_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE)
             .breakByTool(FabricToolTags.PICKAXES, 1)
@@ -55,25 +57,4 @@ public class ModBlocks {
             .requiresTool()
             .strength(3.0F, 3.0F)
             .sounds(BlockSoundGroup.NETHER_ORE), UniformIntProvider.create(2, 6));
-
-    public static final BlockItem SPEEDRUNNER_BLOCK_ITEM = new BlockItem(SPEEDRUNNER_BLOCK,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.MATERIALS));
-
-    public static final BlockItem SPEEDRUNNER_ORE_BLOCK_ITEM = new BlockItem(SPEEDRUNNER_ORE,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.BUILDING_BLOCKS));
-
-    public static final BlockItem DEEPSLATE_SPEEDRUNNER_ORE_BLOCK_ITEM = new BlockItem(DEEPSLATE_SPEEDRUNNER_ORE,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.BUILDING_BLOCKS));
-
-    public static final BlockItem NETHER_SPEEDRUNNER_ORE_BLOCK_ITEM = new BlockItem(NETHER_SPEEDRUNNER_ORE,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.BUILDING_BLOCKS));
-
-    public static final BlockItem IGNEOUS_ORE_BLOCK_ITEM = new BlockItem(IGNEOUS_ORE,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.BUILDING_BLOCKS));
-
-    public static final BlockItem DEEPSLATE_IGNEOUS_ORE_BLOCK_ITEM = new BlockItem(DEEPSLATE_IGNEOUS_ORE,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.BUILDING_BLOCKS));
-
-    public static final BlockItem NETHER_IGNEOUS_ORE_BLOCK_ITEM = new BlockItem(NETHER_IGNEOUS_ORE,
-            new Item.Settings().group(ModItemGroup.SPEEDRUNNER_MOD_ITEM_GROUP).group(ItemGroup.BUILDING_BLOCKS));
 }
