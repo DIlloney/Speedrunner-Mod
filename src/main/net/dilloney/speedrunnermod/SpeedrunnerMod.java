fb148fa7fee335e83d80821fdf12830418c6253c
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-10-16 17:33:49 -0400

    Create SpeedrunnerMod.java

---
+++ src/main/net/dilloney/speedrunnermod/SpeedrunnerMod.java
@@ -0,0 +1,30 @@
+package net.dilloney.speedrunnermod;
+
+import net.dilloney.speedrunnermod.option.ModOptions;
+import net.dilloney.speedrunnermod.option.OptionsFileManager;
+import net.dilloney.speedrunnermod.util.ModRegistry;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SpeedrunnerMod implements ModInitializer {
+
+    private static final String MOD_VERSION = "v1.3.3";
+    private static final String MINECRAFT_VERSION = "1.17.1";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static ModOptions OPTIONS = OptionsFileManager.getMain();
+    public static ModOptions.WorldOptions WORLD_OPTIONS = OptionsFileManager.getWorld();
+    public static ModOptions.MiscOptions MISC_OPTIONS = OptionsFileManager.getMisc();
+
+    public void onInitialize() {
+        ModRegistry.loadOptions();
+        ModRegistry.loadItems();
+        ModRegistry.loadBlocks();
+        ModRegistry.loadConfiguredFeatures();
+        ModRegistry.loadItemTags();
+        ModRegistry.loadBlockTags();
+        ModRegistry.loadMiscellaneous();
+        LOGGER.info("The Speedrunner Mod has been loaded successfully! modVersion = " + MOD_VERSION + " on " + MINECRAFT_VERSION);
+        LOGGER.info("Thank you for using this mod! Enjoy Speedrunning! :)");
+    }
+}
