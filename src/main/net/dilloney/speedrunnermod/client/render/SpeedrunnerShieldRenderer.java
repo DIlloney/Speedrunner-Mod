759e88e40d7edbc80b5db03ebf20de160995544a
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-11-06 16:27:58 -0400

    Add files via upload

---
+++ src/main/net/dilloney/speedrunnermod/client/render/SpeedrunnerShieldRenderer.java
@@ -0,0 +1,49 @@
+package net.dilloney.speedrunnermod.client.render;
+
+import com.mojang.datafixers.util.Pair;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRenderer;
+import net.minecraft.block.entity.BannerBlockEntity;
+import net.minecraft.block.entity.BannerPattern;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BannerBlockEntityRenderer;
+import net.minecraft.client.render.entity.model.ShieldEntityModel;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
+
+@Environment(EnvType.CLIENT)
+public class SpeedrunnerShieldRenderer implements BuiltinItemRenderer {
+
+    private final ShieldEntityModel shieldModel = new ShieldEntityModel();
+
+    public void render(ItemStack stack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        matrices.push();
+        boolean bl = stack.getSubTag("BlockEntityTag") != null;
+        matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(180));
+
+        if (!bl) {
+            VertexConsumer vertexConsumer2 = ItemRenderer.getDirectItemGlintConsumer(vertexConsumers, shieldModel.getLayer(new Identifier("speedrunnermod:textures/entity/speedrunner_shield_base_nopattern.png")), false, stack.hasGlint());
+            shieldModel.render(matrices, vertexConsumer2, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+        } else {
+            SpriteIdentifier spriteIdentifier = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("speedrunnermod:entity/speedrunner_shield_base"));
+            VertexConsumer vertexConsumer = spriteIdentifier.getSprite().getTextureSpecificVertexConsumer(ItemRenderer.getDirectItemGlintConsumer(vertexConsumers, shieldModel.getLayer(ModelLoader.SHIELD_BASE_NO_PATTERN.getAtlasId()), true, stack.hasGlint()));
+            shieldModel.getHandle().render(matrices, vertexConsumer, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+            List<Pair<BannerPattern, DyeColor>> list = BannerBlockEntity.method_24280(ShieldItem.getColor(stack), BannerBlockEntity.getPatternListTag(stack));
+            BannerBlockEntityRenderer.renderCanvas(matrices, vertexConsumers, light, overlay, shieldModel.getPlate(), spriteIdentifier, false, list, stack.hasGlint());
+        }
+
+        matrices.pop();
+    }
+}
