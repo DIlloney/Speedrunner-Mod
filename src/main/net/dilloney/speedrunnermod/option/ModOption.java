e733cfab810e6d1f887ffe66c4808376fc116e7d
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-11-07 13:07:27 -0500

    Update ModOption.java

--- src/main/net/dilloney/speedrunnermod/option/ModOption.java
+++ src/main/net/dilloney/speedrunnermod/option/ModOption.java
@@ -3,6 +3,7 @@ package net.dilloney.speedrunnermod.option;
 import net.dilloney.speedrunnermod.SpeedrunnerMod;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.CyclingOption;
 import net.minecraft.client.option.DoubleOption;
 import net.minecraft.client.resource.language.I18n;
@@ -13,12 +14,15 @@ import net.minecraft.text.TranslatableText;
 public class ModOption {
 
     public static final CyclingOption<Boolean> MAKE_STRUCTURES_MORE_COMMON = CyclingOption.create("options.make_structures_more_common", new TranslatableText("options.make_structures_more_common.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getMakeStructuresMoreCommon(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setMakeStructuresMoreCommon(newValue));
+    public static final CyclingOption<Boolean> MAKE_BIOMES_MORE_COMMON = CyclingOption.create("options.make_biomes_more_common", new TranslatableText("options.make_biomes_more_common.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getMakeBiomesMoreCommon(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setMakeBiomesMoreCommon(newValue));
     public static final CyclingOption<Boolean> ICARUS_MODE = CyclingOption.create("options.icarus_mode", new TranslatableText("options.icarus_mode.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getiCarusMode(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setiCarusMode(newValue));
     public static final CyclingOption<Boolean> INFINITY_PEARL_MODE = CyclingOption.create("options.infini_pearl_mode", new TranslatableText("options.infini_pearl_mode.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getInfinityPearlMode(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setInfiniPearlMode(newValue));
-    public static final CyclingOption<Boolean> FOG = CyclingOption.create("options.fog", new TranslatableText("options.fog.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getFog(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setFog(newValue));
+    public static final CyclingOption<Boolean> FOG = CyclingOption.create("options.fog", new TranslatableText("options.fog.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getFog(), (gameOptions, option, newValue) -> {
+        SpeedrunnerMod.OPTIONS.setFog(newValue);
+        MinecraftClient.getInstance().worldRenderer.reload();
+    });
     public static final CyclingOption<Boolean> TIMER = CyclingOption.create("options.timer", new TranslatableText("options.timer.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getTimer(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setTimer(newValue));
     public static final CyclingOption<Boolean> DOOM_MODE = CyclingOption.create("options.doom_mode", new TranslatableText("options.doom_mode.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getDoomMode(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setDoomMode(newValue));
-    public static final CyclingOption<Boolean> MAKE_BIOMES_MORE_COMMON = CyclingOption.create("options.make_biomes_more_common", new TranslatableText("options.make_biomes_more_common.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getMakeBiomesMoreCommon(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setMakeBiomesMoreCommon(newValue));
     public static final CyclingOption<Boolean> KILL_GHAST_UPON_FIREBALL = CyclingOption.create("options.kill_ghast_upon_fireball", new TranslatableText("options.kill_ghast_upon_fireball.tooltip"), gameOptions -> SpeedrunnerMod.OPTIONS.getKillGhastUponFireball(), (gameOptions, option, newValue) -> SpeedrunnerMod.OPTIONS.setKillGhastUponFireball(newValue));
     public static final DoubleOption STRONGHOLD_COUNT = new DoubleOption("options.stronghold_count", 64.0D, 128.0D, 8.0F, gameOptions -> (double)SpeedrunnerMod.OPTIONS.getStrongholdCount(), (gameOptions, newValue) -> SpeedrunnerMod.OPTIONS.setStrongholdCount(newValue.intValue()), (gameOptions, option) -> {
         return new LiteralText(I18n.translate("options.stronghold_count") + ": " + (int)option.get(gameOptions));
