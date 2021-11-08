94f54d15ca506cb69a5984f51802f62ca4664d72
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-11-08 17:35:06 -0500

    Add files via upload

---
+++ src/main/net/dilloney/speedrunnermod/misc/tag/ModItemTags.java
@@ -0,0 +1,12 @@
+package net.dilloney.speedrunnermod.misc.tag;
+
+import net.minecraft.item.Item;
+import net.minecraft.tag.Tag;
+
+public class ModItemTags {
+
+    public static Tag.Identified<Item> IRON_INGOTS;
+    public static Tag.Identified<Item> IRON_NUGGETS;
+    public static Tag.Identified<Item> IRON_BLOCKS;
+    public static Tag.Identified<Item> COBBLESTONES;
+}
