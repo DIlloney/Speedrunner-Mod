3ec76b84d5223c7745c0961ffb2a1196e66812d9
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-12-05 13:38:42 -0500

    Add files via upload

---
+++ src/main/net/dilloney/speedrunnermod/option/clModOptions.java
@@ -0,0 +1,73 @@
+package net.dilloney.speedrunnermod.option;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.util.math.MathHelper;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+
+@Environment(EnvType.CLIENT)
+public class clModOptions {
+
+    public boolean fog = true;
+    public boolean timer = false;
+    public boolean autoCreateWorld = true;
+    public WorldDifficulty worldDifficulty = WorldDifficulty.EASY;
+
+    public boolean getFog() {
+        return this.fog;
+    }
+
+    public boolean getTimer() {
+        return this.timer;
+    }
+
+    public boolean getAutoCreateWorld() {
+        return this.autoCreateWorld;
+    }
+
+    public void setFog(boolean fog) {
+        this.fog = fog;
+    }
+
+    public void setTimer(boolean timer) {
+        this.timer = timer;
+    }
+
+    public void setAutoCreateWorld(boolean autoCreateWorld) {
+        this.autoCreateWorld = autoCreateWorld;
+    }
+
+    public enum WorldDifficulty {
+        PEACEFUL(0, "options.difficulty.peaceful"),
+        EASY(1, "options.difficulty.easy"),
+        NORMAL(2, "options.difficulty.normal"),
+        HARD(3, "options.difficulty.hard"),
+        HARDCORE(4, "options.difficulty.hardcore");
+
+        private static final WorldDifficulty[] VALUES = (WorldDifficulty[])Arrays.stream(values()).sorted(Comparator.comparingInt(WorldDifficulty::getId)).toArray((i) -> {
+            return new WorldDifficulty[i];
+        });
+        private final int id;
+        private final String translateKey;
+
+        WorldDifficulty(int id, String translationKey) {
+            this.id = id;
+            this.translateKey = Objects.requireNonNull(translationKey, "translateKey");
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public String getTranslationKey() {
+            return this.translateKey;
+        }
+
+        public static clModOptions.WorldDifficulty byId(int id) {
+            return VALUES[MathHelper.floorMod(id, VALUES.length)];
+        }
+    }
+}
