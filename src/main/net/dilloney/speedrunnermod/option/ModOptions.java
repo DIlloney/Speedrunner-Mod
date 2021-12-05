b0438b2ddf634ef106ca9a5d9d42a6f4d8832e24
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-12-05 13:32:38 -0500

    Add files via upload

--- src/main/net/dilloney/speedrunnermod/option/ModOptions.java
+++ src/main/net/dilloney/speedrunnermod/option/ModOptions.java
@@ -1,142 +1,143 @@
 package net.dilloney.speedrunnermod.option;
 
-import net.minecraft.util.math.MathHelper;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
 public class ModOptions {
+    public final Main main = new Main();
+    public final Advanced advanced = new Advanced();
 
-    public boolean makeStructuresMoreCommon = true;
-    public boolean makeBiomesMoreCommon = true;
-    public boolean iCarusMode = false;
-    public boolean infiniPearlMode = false;
-    public boolean fog = true;
-    public boolean timer = false;
-    public boolean doomMode = false;
-    public boolean killGhastUponFireball = false;
-    public int strongholdCount = 128;
-    public int dragonPerchTime = 30;
-    public boolean autoCreateWorld = true;
-    public WorldDifficulty worldDifficulty = WorldDifficulty.EASY;
+    public static class Main {
 
-    protected boolean getMakeStructuresMoreCommon() {
-        return this.makeStructuresMoreCommon;
-    }
+        public boolean commonStructures = true;
+        public boolean commonBiomes = false;
+        public boolean iCarusMode = false;
+        public boolean infiniPearlMode = false;
+        public boolean doomMode = false;
+        public boolean modifyBlockHardness = true;
+        public boolean killGhastUponFireball = false;
+        public int strongholdCount = 128;
+        public int dragonPerchTime = 30;
 
-    protected boolean getMakeBiomesMoreCommon() {
-        return this.makeBiomesMoreCommon;
-    }
-
-    protected boolean getiCarusMode() {
-        return this.iCarusMode;
-    }
-
-    protected boolean getInfinityPearlMode() {
-        return this.infiniPearlMode;
-    }
-
-    protected boolean getFog() {
-        return this.fog;
-    }
-
-    protected boolean getTimer() {
-        return this.timer;
-    }
-
-    protected boolean getDoomMode() {
-        return this.doomMode;
-    }
-
-    protected boolean getKillGhastUponFireball() {
-        return this.killGhastUponFireball;
-    }
-
-    public int getStrongholdCount() {
-        return this.strongholdCount;
-    }
-
-    public int getDragonPerchTime() {
-        return this.dragonPerchTime;
-    }
-
-    protected boolean getAutoCreateWorld() {
-        return this.autoCreateWorld;
-    }
-
-    protected void setMakeStructuresMoreCommon(boolean makeStructuresMoreCommon) {
-        this.makeStructuresMoreCommon = makeStructuresMoreCommon;
-    }
-
-    protected void setMakeBiomesMoreCommon(boolean makeBiomesMoreCommon) {
-        this.makeBiomesMoreCommon = makeBiomesMoreCommon;
-    }
-
-    protected void setiCarusMode(boolean iCarusMode) {
-        this.iCarusMode = iCarusMode;
-    }
-
-    protected void setInfiniPearlMode(boolean infiniPearlMode) {
-        this.infiniPearlMode = infiniPearlMode;
-    }
-
-    protected void setFog(boolean fog) {
-        this.fog = fog;
-    }
-
-    protected void setTimer(boolean timer) {
-        this.timer = timer;
-    }
-
-    protected void setDoomMode(boolean doomMode) {
-        this.doomMode = doomMode;
-    }
-
-    protected void setKillGhastUponFireball(boolean killGhastUponFireball) {
-        this.killGhastUponFireball = killGhastUponFireball;
-    }
-
-    public void setStrongholdCount(int strongholdCount) {
-        this.strongholdCount = strongholdCount;
-    }
-
-    protected void setDragonPerchTime(int dragonPerchTime) {
-        this.dragonPerchTime = dragonPerchTime;
-    }
-
-    protected void setAutoCreateWorld(boolean autoCreateWorld) {
-        this.autoCreateWorld = autoCreateWorld;
-    }
-
-    public enum WorldDifficulty {
-        PEACEFUL(0, "options.difficulty.peaceful"),
-        EASY(1, "options.difficulty.easy"),
-        NORMAL(2, "options.difficulty.normal"),
-        HARD(3, "options.difficulty.hard"),
-        HARDCORE(4, "options.difficulty.hardcore");
-
-        private static final WorldDifficulty[] VALUES = (WorldDifficulty[])Arrays.stream(values()).sorted(Comparator.comparingInt(WorldDifficulty::getId)).toArray((i) -> {
-            return new WorldDifficulty[i];
-        });
-        private final int id;
-        private final String translateKey;
-
-        WorldDifficulty(int id, String translationKey) {
-            this.id = id;
-            this.translateKey = Objects.requireNonNull(translationKey, "translateKey");
+        public boolean getCommonStructures() {
+            return this.commonStructures;
         }
 
-        public int getId() {
-            return this.id;
+        public boolean getCommonBiomes() {
+            return commonBiomes;
         }
 
-        public String getTranslationKey() {
-            return this.translateKey;
+        public boolean getiCarusMode() {
+            return this.iCarusMode;
         }
 
-        public static WorldDifficulty byId(int id) {
-            return VALUES[MathHelper.floorMod(id, VALUES.length)];
+        public boolean getInfinityPearlMode() {
+            return this.infiniPearlMode;
+        }
+
+        public boolean getDoomMode() {
+            return this.doomMode;
+        }
+
+        public boolean getModifyBlockHardness() {
+            return this.modifyBlockHardness;
+        }
+
+        public boolean getKillGhastUponFireball() {
+            return this.killGhastUponFireball;
+        }
+
+        public int getStrongholdCount() {
+            return this.strongholdCount;
+        }
+
+        public int getDragonPerchTime() {
+            return this.dragonPerchTime;
+        }
+
+        public void setCommonStructures(boolean commonStructures) {
+            this.commonStructures = commonStructures;
+        }
+
+        public void setCommonBiomes(boolean commonBiomes) {
+            this.commonBiomes = commonBiomes;
+        }
+
+        public void setiCarusMode(boolean iCarusMode) {
+            this.iCarusMode = iCarusMode;
+        }
+
+        public void setInfiniPearlMode(boolean infiniPearlMode) {
+            this.infiniPearlMode = infiniPearlMode;
+        }
+
+        public void setDoomMode(boolean doomMode) {
+            this.doomMode = doomMode;
+        }
+
+        public void setModifyBlockHardness(boolean modifyBlockHardness) {
+            this.modifyBlockHardness = modifyBlockHardness;
+        }
+
+        public void setKillGhastUponFireball(boolean killGhastUponFireball) {
+            this.killGhastUponFireball = killGhastUponFireball;
+        }
+
+        public void setStrongholdCount(int strongholdCount) {
+            this.strongholdCount = strongholdCount;
+        }
+
+        public void setDragonPerchTime(int dragonPerchTime) {
+            this.dragonPerchTime = dragonPerchTime;
+        }
+    }
+
+    public static class Advanced {
+
+        public MobSpawningRate mobSpawningRate = MobSpawningRate.HIGH;
+        public boolean modifyBiomes = true;
+        public int strongholdDistance = 4;
+
+        public boolean getModifyBiomes() {
+            return this.modifyBiomes;
+        }
+
+        public int getStrongholdDistance() {
+            return this.strongholdDistance;
+        }
+
+        public void setModifyBiomes(boolean modifyBiomes) {
+            this.modifyBiomes = modifyBiomes;
+        }
+
+        public void setStrongholdDistance(int strongholdDistance) {
+            this.strongholdDistance = strongholdDistance;
+        }
+
+        public enum MobSpawningRate {
+            LOW(0, "options.mob_spawning_rate.low"),
+            NORMAL(1, "options.mob_spawning_rate.normal"),
+            HIGH(2, "options.mob_spawning_rate.high");
+
+            private static final MobSpawningRate[] VALUES = (MobSpawningRate[])Arrays.stream(values()).sorted(Comparator.comparingInt(MobSpawningRate::getId)).toArray((i) -> {
+                return new MobSpawningRate[i];
+            });
+            private final int id;
+            private final String translateKey;
+
+            MobSpawningRate(int id, String translationKey) {
+                this.id = id;
+                this.translateKey = Objects.requireNonNull(translationKey, "translateKey");
+            }
+
+            public int getId() {
+                return this.id;
+            }
+
+            public String getTranslationKey() {
+                return this.translateKey;
+            }
         }
     }
 }
