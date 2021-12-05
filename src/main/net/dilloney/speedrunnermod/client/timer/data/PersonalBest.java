ede809bbd65949c1347e827e1fb5a46cf6c842c7
Dilloney <79867677+Dilloney@users.noreply.github.com>
2021-12-05 13:31:01 -0500

    Add files via upload

---
+++ src/main/net/dilloney/speedrunnermod/client/timer/data/PersonalBest.java
@@ -0,0 +1,14 @@
+package net.dilloney.speedrunnermod.client.timer.data;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class PersonalBest extends AbstractRun {
+
+    public void tryRun(AbstractRun otherRun) {
+        if (otherRun.finishedSplitTicks == UNINITIALIZED || (finishedSplitTicks != UNINITIALIZED && finishedSplitTicks <= otherRun.finishedSplitTicks)) return;
+        finishedSplitTicks = otherRun.finishedSplitTicks;
+        finishedRealTime = otherRun.finishedRealTime;
+    }
+}
