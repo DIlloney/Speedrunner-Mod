924e566ae0e97ac5d988f9963db7f69367149af1
Dilloney <79867677+Dilloney@users.noreply.github.com>
2022-01-14 11:44:04 -0500

    Add files via upload

--- src/main/net/dilloney/speedrunnermod/SpeedrunnerMod.java
+++ src/main/net/dilloney/speedrunnermod/SpeedrunnerMod.java
@@ -1,9 +1,6 @@
 package net.dilloney.speedrunnermod;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import net.dilloney.speedrunnermod.block.ModBlocks;
 import net.dilloney.speedrunnermod.item.ModItems;
 import net.dilloney.speedrunnermod.option.ModOptions;
@@ -13,47 +10,30 @@ import net.dilloney.speedrunnermod.tag.ModItemTags;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.mixin.structure.StructuresConfigAccessor;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.chunk.StructureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SpeedrunnerMod implements ModInitializer {
-    public static final Text SPEEDRUNNER_MOD_TITLE = new TranslatableText("speedrunnermod.title");
-    public static final Identifier SPEEDRUNNER_BOOTS = new Identifier(SpeedrunnerMod.MOD_ID, "textures/item/speedrunner_boots.png");
-    public static final Identifier SPEEDRUNNER_INGOT = new Identifier(SpeedrunnerMod.MOD_ID, "textures/item/speedrunner_ingot.png");
     public static final String MOD_ID = "speedrunnermod";
-    public static final String MOD_VERSION = "v1.5.2";
-    public static final String MINECRAFT_VERSION = "1.18.1";
+    public static final String MOD_VERSION = "v1.6";
     public static final Logger LOGGER = LogManager.getLogger();
-    private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
-    private static final String CONFIG = "speedrunnermod-options.json";
-    private static File file;
-    private static ModOptions OPTIONS = getConfig();
-    private static final String OLD_CONFIG = "speedrunnermod_options.json";
-    private static File old_config_file;
 
     public void onInitialize() {
-        SpeedrunnerMod.loadConfig();
+        ModOptions.loadConfig();
 
         ModItems.init();
         ModBlocks.init();
         ModItemTags.init();
         ModBlockTags.init();
-        SpeedrunnerShieldDecorationRecipe.register();
+        SpeedrunnerShieldDecorationRecipe.init();
 
-        if (SpeedrunnerMod.options().main.makeStructuresMoreCommon) {
-            SpeedrunnerMod.makeStructuresMoreCommon();
+        if (options().main.makeStructuresMoreCommon) {
+            makeStructuresMoreCommon();
         }
 
         LOGGER.info("The Speedrunner Mod (" + MOD_VERSION + ")" + " has been loaded.");
@@ -88,76 +68,14 @@ public class SpeedrunnerMod implements ModInitializer {
             ImmutableMap<StructureFeature<?>, StructureConfig> immutableMap = ImmutableMap.copyOf(map);
 
             ((StructuresConfigAccessor)world.getChunkManager().getChunkGenerator().getStructuresConfig()).setStructures(immutableMap);
+
+            if (options().advanced.debugMode) {
+                LOGGER.debug("Structures have been made more common");
+            }
         });
     }
 
-    private static void loadConfig() {
-        File configFile = getConfigFile();
-        File oldConfigFile = getOldConfigFile();
-        if (oldConfigFile.exists()) {
-            oldConfigFile.delete();
-            LOGGER.warn("Found an old configuration file, deleting.");
-        }
-
-        if (!configFile.exists()) {
-            SpeedrunnerMod.OPTIONS = new ModOptions();
-        } else {
-            sanitize();
-            readConfig();
-        }
-        saveConfig();
-    }
-
-    private static void readConfig() {
-        SpeedrunnerMod.OPTIONS = getConfig();
-    }
-
-    public static void saveConfig() {
-        File file = getConfigFile();
-        try (FileWriter writer = new FileWriter(file)) {
-            writer.write(GSON.toJson(SpeedrunnerMod.options()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void setConfig(ModOptions config) {
-        SpeedrunnerMod.OPTIONS = config;
-        saveConfig();
-    }
-
-    private static ModOptions getConfig() {
-        File file = getConfigFile();
-        try (FileReader reader = new FileReader(file)) {
-            return GSON.fromJson(reader, ModOptions.class);
-        } catch (Exception e) {
-            ModOptions newconfig = new ModOptions();
-            setConfig(newconfig);
-            return newconfig;
-        }
-    }
-
-    private static File getConfigFile() {
-        if (file == null) {
-            file = new File(FabricLoader.getInstance().getConfigDir().toFile(), CONFIG);
-        }
-        return file;
-    }
-
-    private static void sanitize() {
-        if (SpeedrunnerMod.options().advanced.mobSpawningRate == null) {
-            SpeedrunnerMod.options().advanced.mobSpawningRate = ModOptions.Advanced.MobSpawningRate.HIGH;
-        }
-    }
-
     public static ModOptions options() {
-        return OPTIONS;
-    }
-
-    private static File getOldConfigFile() {
-        if (old_config_file == null) {
-            old_config_file = new File(FabricLoader.getInstance().getConfigDir().toFile(), OLD_CONFIG);
-        }
-        return old_config_file;
+        return ModOptions.OPTIONS;
     }
 }
--- src/main/net/dilloney/speedrunnermod/SpeedrunnerModClient.java
+++ src/main/net/dilloney/speedrunnermod/SpeedrunnerModClient.java
@@ -1,92 +1,25 @@
 package net.dilloney.speedrunnermod;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import net.dilloney.speedrunnermod.client.timer.Timer;
 import net.dilloney.speedrunnermod.item.ModItems;
-import net.dilloney.speedrunnermod.option.clModOptions;
+import net.dilloney.speedrunnermod.option.CLModOptions;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.loader.api.FabricLoader;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 
 @Environment(EnvType.CLIENT)
 public class SpeedrunnerModClient implements ClientModInitializer {
-    private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
-    private static final String CONFIG = "speedrunnermod-cloptions.json";
-    private static File file;
-    private static clModOptions CLOPTIONS = getClientConfig();
 
     public void onInitializeClient() {
-        loadClientConfig();
+        CLModOptions.loadClientConfig();
 
         ModItems.clinit();
-        Timer.init();
     }
 
-    private static void loadClientConfig() {
-        File file = getClientConfigFile();
-        if (!file.exists()) {
-            SpeedrunnerModClient.CLOPTIONS = new clModOptions();
-        } else {
-            sanitize();
-            readClientConfig();
-        }
-        saveClientConfig();
+    public static CLModOptions clOptions() {
+        return CLModOptions.CLOPTIONS;
     }
 
-    private static void readClientConfig() {
-        SpeedrunnerModClient.CLOPTIONS = getClientConfig();
-    }
-
-    public static void saveClientConfig() {
-        File file = getClientConfigFile();
-        try (FileWriter writer = new FileWriter(file)) {
-            writer.write(GSON.toJson(SpeedrunnerModClient.clOptions()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void setClientConfig(clModOptions config) {
-        SpeedrunnerModClient.CLOPTIONS = config;
-        saveClientConfig();
-    }
-
-    private static clModOptions getClientConfig() {
-        File file = getClientConfigFile();
-        try (FileReader reader = new FileReader(file)) {
-            return GSON.fromJson(reader, clModOptions.class);
-        } catch (Exception e) {
-            clModOptions newconfig = new clModOptions();
-            setClientConfig(newconfig);
-            return newconfig;
-        }
-    }
-
-    private static File getClientConfigFile() {
-        if (file == null) {
-            file = new File(FabricLoader.getInstance().getConfigDir().toFile(), CONFIG);
-        }
-        return file;
-    }
-
-    private static void sanitize() {
-        if (SpeedrunnerModClient.clOptions().worldDifficulty == null) {
-            SpeedrunnerModClient.clOptions().worldDifficulty = clModOptions.WorldDifficulty.EASY;
-        }
-
-        if (SpeedrunnerModClient.clOptions().modButtonType == null) {
-            SpeedrunnerModClient.clOptions().modButtonType = clModOptions.ModButtonType.LOGO;
-        }
-    }
-
-    public static clModOptions clOptions() {
-        return CLOPTIONS;
+    public static void logException(Exception ex, String message) {
+        System.err.printf("[Speedrunner Mod] %s (%s: %s)", message, ex.getClass().getSimpleName(), ex.getLocalizedMessage());
     }
 }
