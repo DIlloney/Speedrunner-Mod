31fe08ab39f2653ecbf6d81c733117c01a1326c7
Dilloney <79867677+Dilloney@users.noreply.github.com>
2022-01-14 11:42:35 -0500

    Add files via upload

--- src/main/net/dilloney/speedrunnermod/option/ModOptions.java
+++ src/main/net/dilloney/speedrunnermod/option/ModOptions.java
@@ -1,16 +1,32 @@
 package net.dilloney.speedrunnermod.option;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.dilloney.speedrunnermod.SpeedrunnerMod;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
 public class ModOptions {
+    private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
+    private static final String CONFIG = "speedrunnermod-options.json";
+    private static File file;
+    public static ModOptions OPTIONS = getConfig();
+    private static final String OLD_CONFIG = "speedrunnermod_options.json";
+    private static File old_config_file;
     public final Main main = new Main();
     public final Advanced advanced = new Advanced();
 
     public static class Main {
 
         public boolean makeStructuresMoreCommon = true;
+        public boolean makeBiomesMoreCommon = true;
         public boolean iCarusMode = false;
         public boolean infiniPearlMode = false;
         public boolean doomMode = false;
@@ -18,93 +34,18 @@ public class ModOptions {
         public boolean killGhastUponFireball = false;
         public int strongholdCount = 128;
         public int dragonPerchTime = 30;
-
-        public boolean getMakeStructuresMoreCommon() {
-            return this.makeStructuresMoreCommon;
-        }
-
-        public boolean getiCarusMode() {
-            return this.iCarusMode;
-        }
-
-        public boolean getInfinityPearlMode() {
-            return this.infiniPearlMode;
-        }
-
-        public boolean getDoomMode() {
-            return this.doomMode;
-        }
-
-        public boolean getModifiedBlockHardness() {
-            return this.modifiedBlockHardness;
-        }
-
-        public boolean getKillGhastUponFireball() {
-            return this.killGhastUponFireball;
-        }
-
-        public int getStrongholdCount() {
-            return this.strongholdCount;
-        }
-
-        public int getDragonPerchTime() {
-            return this.dragonPerchTime;
-        }
-
-        public void setMakeStructuresMoreCommon(boolean makeStructuresMoreCommon) {
-            this.makeStructuresMoreCommon = makeStructuresMoreCommon;
-        }
-
-        public void setiCarusMode(boolean iCarusMode) {
-            this.iCarusMode = iCarusMode;
-        }
-
-        public void setInfiniPearlMode(boolean infiniPearlMode) {
-            this.infiniPearlMode = infiniPearlMode;
-        }
-
-        public void setDoomMode(boolean doomMode) {
-            this.doomMode = doomMode;
-        }
-
-        public void setModifiedBlockHardness(boolean modifiedBlockHardness) {
-            this.modifiedBlockHardness = modifiedBlockHardness;
-        }
-
-        public void setKillGhastUponFireball(boolean killGhastUponFireball) {
-            this.killGhastUponFireball = killGhastUponFireball;
-        }
-
-        public void setStrongholdCount(int strongholdCount) {
-            this.strongholdCount = strongholdCount;
-        }
-
-        public void setDragonPerchTime(int dragonPerchTime) {
-            this.dragonPerchTime = dragonPerchTime;
-        }
     }
 
     public static class Advanced {
 
         public MobSpawningRate mobSpawningRate = MobSpawningRate.HIGH;
-        public boolean modifyBiomes = true;
+        public boolean modifiedBiomes = true;
+        public boolean modifiedFoods = true;
+        public boolean modifiedItemEffects = true;
+        public boolean makeOresMoreCommon = true;
+        public int mobSpawnerSpawnDuration = 40;
         public int strongholdDistance = 4;
-
-        public boolean getModifyBiomes() {
-            return this.modifyBiomes;
-        }
-
-        public int getStrongholdDistance() {
-            return this.strongholdDistance;
-        }
-
-        public void setModifyBiomes(boolean modifyBiomes) {
-            this.modifyBiomes = modifyBiomes;
-        }
-
-        public void setStrongholdDistance(int strongholdDistance) {
-            this.strongholdDistance = strongholdDistance;
-        }
+        public boolean debugMode = false;
 
         public enum MobSpawningRate {
             LOW(0, "options.mob_spawning_rate.low"),
@@ -130,5 +71,71 @@ public class ModOptions {
                 return this.translateKey;
             }
         }
+    }
+
+    public static void loadConfig() {
+        File configFile = getConfigFile();
+        File oldConfigFile = getOldConfigFile();
+        if (oldConfigFile.exists()) {
+            oldConfigFile.delete();
+            SpeedrunnerMod.LOGGER.warn("Found an old configuration file, deleting.");
+        }
+
+        if (!configFile.exists()) {
+            OPTIONS = new ModOptions();
+        } else {
+            sanitize();
+            readConfig();
+        }
+        saveConfig();
+    }
+
+    private static void readConfig() {
+        OPTIONS = getConfig();
+    }
+
+    public static void saveConfig() {
+        File file = getConfigFile();
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(GSON.toJson(SpeedrunnerMod.options()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void setConfig(ModOptions config) {
+        OPTIONS = config;
+        saveConfig();
+    }
+
+    private static ModOptions getConfig() {
+        File file = getConfigFile();
+        try (FileReader reader = new FileReader(file)) {
+            return GSON.fromJson(reader, ModOptions.class);
+        } catch (Exception e) {
+            ModOptions newconfig = new ModOptions();
+            setConfig(newconfig);
+            return newconfig;
+        }
+    }
+
+    private static File getConfigFile() {
+        if (file == null) {
+            file = new File(FabricLoader.getInstance().getConfigDir().toFile(), CONFIG);
+        }
+        return file;
+    }
+
+    private static void sanitize() {
+        if (SpeedrunnerMod.options().advanced.mobSpawningRate == null) {
+            SpeedrunnerMod.options().advanced.mobSpawningRate = ModOptions.Advanced.MobSpawningRate.HIGH;
+        }
+    }
+
+    private static File getOldConfigFile() {
+        if (old_config_file == null) {
+            old_config_file = new File(FabricLoader.getInstance().getConfigDir().toFile(), OLD_CONFIG);
+        }
+        return old_config_file;
     }
 }
