1015c3fbfec6c0e832e13f14849d8d2fe1785aa7
Dilloney <79867677+Dilloney@users.noreply.github.com>
2022-01-14 11:49:11 -0500

    Add files via upload

---
+++ src/main/net/dilloney/speedrunnermod/client/gui/hud/DebugModText.java
@@ -0,0 +1,23 @@
+package net.dilloney.speedrunnermod.client.gui.hud;
+
+import net.dilloney.speedrunnermod.SpeedrunnerMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Environment(EnvType.CLIENT)
+@Mixin(DebugHud.class)
+public class DebugModText {
+
+    @Inject(method = "getRightText", at = @At("RETURN"), cancellable = true)
+    private void getRightText(CallbackInfoReturnable<List<String>> cir) {
+        List<String> returnValue = (List<String>)cir.getReturnValue();
+        returnValue.add("Speedrunner Mod: " + SpeedrunnerMod.MOD_VERSION);
+    }
+}
