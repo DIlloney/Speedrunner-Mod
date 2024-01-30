a968ef9d8b3ba17fa19bacdaf95b07630877a6d3
Dillon <79867677+Dillon8775@users.noreply.github.com>
2024-01-29 21:35:43 -0500

    Add files via upload

---
+++ src/main/java/net/dillon8775/speedrunnermod/client/gui/screen/features/blocks_and_items/SpeedrunnersEyeScreen.java
@@ -0,0 +1,72 @@
+package net.dillon8775.speedrunnermod.client.gui.screen.features.blocks_and_items;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.dillon8775.speedrunnermod.client.gui.screen.FeaturesScreen;
+import net.dillon8775.speedrunnermod.client.gui.screen.text.ModScreenTexts;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ScreenTexts;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+
+public class SpeedrunnersEyeScreen extends GameOptionsScreen {
+    private final Screen parent;
+    protected final LiteralText pageNumber = new LiteralText("§lPage:§r 9/12");
+
+    public SpeedrunnersEyeScreen(Screen parent, GameOptions options) {
+        super(parent, options, new TranslatableText("speedrunnermod.title.features.blocks_and_items.speedrunners_eye"));
+        this.parent = parent;
+    }
+
+    protected void init() {
+        int leftSide = this.width / 2 - 155;
+        int rightSide = leftSide + 160;
+        int farLeftSide = leftSide - 80;
+        int farRightSide = rightSide + 80;
+        int middle = this.width / 2 - 75;
+        int height = this.height - 29;
+
+        this.addDrawableChild(new ButtonWidget(farRightSide, height, 150, 20, ModScreenTexts.NEXT, (buttonWidget) -> {
+            this.client.setScreen(new SpeedrunnerBulkScreen(this.parent, MinecraftClient.getInstance().options));
+        }));
+
+        this.addDrawableChild(new ButtonWidget(farLeftSide, height, 150, 20, ModScreenTexts.PREVIOUS, (buttonWidget) -> {
+            this.client.setScreen(new EyeOfAnnulScreen(this.parent, MinecraftClient.getInstance().options));
+        }));
+
+        this.addDrawableChild(new ButtonWidget(middle, height, 150, 20, ScreenTexts.DONE, (button) -> {
+            this.client.setScreen(new FeaturesScreen(this.parent, MinecraftClient.getInstance().options));
+        }));
+    }
+
+    public void onClose() {
+        this.client.setScreen(new FeaturesScreen(this.parent, MinecraftClient.getInstance().options));
+    }
+
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 15, 16777215);
+        drawCenteredText(matrices, this.textRenderer, new TranslatableText("speedrunnermod.features.blocks_and_items.speedrunners_eye.line1"), this.width / 2, 90, 16777215);
+        drawCenteredText(matrices, this.textRenderer, new TranslatableText("speedrunnermod.features.blocks_and_items.speedrunners_eye.line2"), this.width / 2, 110, 16777215);
+
+        int leftSide = this.width / 2 - 155;
+        int rightSide = leftSide + 160;
+        int farRightSide = rightSide + 273;
+        int height = this.height - 24;
+        drawCenteredText(matrices, this.textRenderer, this.pageNumber, farRightSide, height, 16777215);
+
+        leftSide = this.width / 2 - 155;
+        rightSide = leftSide + 160;
+        height = this.height / 6 + 126;
+        RenderSystem.setShaderTexture(0, new Identifier("speedrunnermod:textures/gui/features/blocks_and_items/speedrunners_eye_recipe.png"));
+        drawTexture(matrices, leftSide, height, 0.0F, 0.0F, 90, 90, 90, 90);
+        RenderSystem.setShaderTexture(0, new Identifier("speedrunnermod:textures/gui/features/blocks_and_items/speedrunners_eye.png"));
+        drawTexture(matrices, rightSide, height, 0.0F, 0.0F, 160, 90, 160, 90);
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+}
