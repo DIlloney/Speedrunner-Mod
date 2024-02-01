dbdf8d1d3cc6a948a0c97723b6029a84f9cdb539
Dillon <79867677+Dillon8775@users.noreply.github.com>
2024-01-31 21:50:13 -0500

    Delete src/main/java/net/dillon8775/speedrunnermod directory

--- src/main/java/net/dillon8775/speedrunnermod/util/ModSignType.java
+++
@@ -1,7 +0,0 @@
-package net.dillon8775.speedrunnermod.util;
-
-import net.minecraft.util.SignType;
-
-public class ModSignType {
-    public static final SignType SPEEDRUNNER = SignType.register(new SignType("speedrunner"));
-}
