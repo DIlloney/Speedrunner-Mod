20e8545f31ef0f7336fef2bcb25ec7414acbed9b
Dillon <79867677+Dillon8775@users.noreply.github.com>
2024-02-29 11:18:52 -0500

    1.18.1 code

---
+++ src/main/java/net/dillon8775/speedrunnermod/mixin/client/Keybindings.java
@@ -0,0 +1,90 @@
+package net.dillon8775.speedrunnermod.mixin.client;
+
+import net.dillon8775.speedrunnermod.SpeedrunnerModClient;
+import net.dillon8775.speedrunnermod.option.ClientModOptions;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.SaveLevelScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.dillon8775.speedrunnermod.SpeedrunnerMod.debug;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MinecraftClient.class)
+public abstract class Keybindings {
+    @Shadow @Final
+    public InGameHud inGameHud;
+    @Shadow
+    public ClientWorld world;
+    @Shadow
+    abstract void disconnect(Screen screen);
+
+    @Shadow public abstract void setScreen(@Nullable Screen screen);
+
+    /**
+     * Applies the {@code speedrunner mod keybinds} to the game.
+     */
+    @Inject(at = @At("TAIL"), method = "handleInputEvents")
+    private void handleInputEvents(CallbackInfo info) {
+        while (ClientModOptions.resetKey.wasPressed()) {
+            if (SpeedrunnerModClient.clientOptions().fastWorldCreation) {
+                if (this.inGameHud != null) {
+                    this.inGameHud.getChatHud().clear(false);
+                }
+                assert this.world != null;
+                this.world.disconnect();
+                this.disconnect(new SaveLevelScreen(new TranslatableText("menu.savingLevel")));
+                this.setScreen(CreateWorldScreen.create(null));
+            } else {
+                debugWarnSpeedrunnerMod("\"Fast World Creation\" is OFF, please enable to use this feature.");
+            }
+        }
+
+        while (ClientModOptions.fogKey.wasPressed()) {
+            SpeedrunnerModClient.clientOptions().fog = !SpeedrunnerModClient.clientOptions().fog;
+            ClientModOptions.saveClientConfig();
+            MinecraftClient.getInstance().worldRenderer.reload();
+            debugWarnSpeedrunnerMod(SpeedrunnerModClient.clientOptions().fog ? "speedrunnermod.toggle_fog.on" : "speedrunnermod.toggle_fog.off");
+        }
+
+        while (ClientModOptions.hitboxesKey.wasPressed()) {
+            boolean bl = !MinecraftClient.getInstance().getEntityRenderDispatcher().shouldRenderHitboxes();
+            String hitboxes = bl ? "ON" : "OFF";
+            MinecraftClient.getInstance().getEntityRenderDispatcher().setRenderHitboxes(bl);
+            debugWarn(bl ? "debug.show_hitboxes.on" : "debug.show_hitboxes.off");
+            debug("Toggled hitboxes " + hitboxes);
+        }
+
+        while (ClientModOptions.chunkBordersKey.wasPressed()) {
+            boolean bl = MinecraftClient.getInstance().debugRenderer.toggleShowChunkBorder();
+            String chunkBorders = bl ? "ON" : "OFF";
+            debugWarn(bl ? "debug.chunk_boundaries.on" : "debug.chunk_boundaries.off");
+            debug("Toggled chunk borders " + chunkBorders);
+        }
+    }
+
+    @Unique
+    private void debugWarn(String string, Object... objects) {
+        this.inGameHud.getChatHud().addMessage((new LiteralText("")).append((new TranslatableText("debug.prefix")).formatted(Formatting.YELLOW, Formatting.BOLD)).append(" ").append(new TranslatableText(string, objects)));
+    }
+
+    @Unique
+    private void debugWarnSpeedrunnerMod(String string, Object... objects) {
+        this.inGameHud.getChatHud().addMessage((new LiteralText("")).append((new TranslatableText("speedrunnermod.debug.prefix")).formatted(Formatting.AQUA, Formatting.BOLD)).append(" ").append(new TranslatableText(string, objects)));
+    }
+}
---
+++ src/main/java/net/dillon8775/speedrunnermod/mixin/client/hud/DebugModText.java
@@ -0,0 +1,28 @@
+package net.dillon8775.speedrunnermod.mixin.client.hud;
+
+import net.dillon8775.speedrunnermod.SpeedrunnerMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+import static net.dillon8775.speedrunnermod.SpeedrunnerMod.DOOM_MODE;
+
+@Environment(EnvType.CLIENT)
+@Mixin(DebugHud.class)
+public class DebugModText {
+
+    @Inject(method = "getRightText", at = @At("RETURN"), cancellable = true)
+    private void getRightText(CallbackInfoReturnable<List<String>> cir) {
+        List<String> returnValue = cir.getReturnValue();
+        returnValue.add(SpeedrunnerMod.THE_SPEEDRUNNER_MOD_STRING + " " + SpeedrunnerMod.MOD_VERSION);
+        if (DOOM_MODE) {
+            returnValue.add("What's that? Doom Mode? Oh, flip.");
+        }
+    }
+}
