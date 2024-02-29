cb64104df0f2ceb67b550000c94a63e97600653c
Dillon <79867677+Dillon8775@users.noreply.github.com>
2024-02-29 11:13:12 -0500

    1.18.2 code

---
+++ src/main/java/net/dillon8775/speedrunnermod/client/screen/features/blocks_and_items/SpeedrunnersEyeScreen.java
@@ -0,0 +1,70 @@
+package net.dillon8775.speedrunnermod.client.screen.features.blocks_and_items;
+
+import net.dillon8775.speedrunnermod.SpeedrunnerModClient;
+import net.dillon8775.speedrunnermod.client.screen.FeaturesScreen;
+import net.dillon8775.speedrunnermod.client.screen.features.ScreenType;
+import net.dillon8775.speedrunnermod.client.screen.features.ScreenTypes;
+import net.dillon8775.speedrunnermod.client.util.ModTexts;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ScreenTexts;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+
+@ScreenType(ScreenTypes.NORMAL)
+@Environment(EnvType.CLIENT)
+public class SpeedrunnersEyeScreen extends GameOptionsScreen {
+    private final Screen parent;
+    protected final LiteralText pageNumber = new LiteralText("§lPage:§r 9/12");
+
+    public SpeedrunnersEyeScreen(Screen parent, GameOptions options) {
+        super(parent, options, new TranslatableText("speedrunnermod.title.features.blocks_and_items.speedrunners_eye"));
+        this.parent = parent;
+    }
+
+    @Override
+    protected void init() {
+        int middle = this.width / 2 - 75;
+        int height = this.height / 6 + 150;
+
+        this.addDrawableChild(new ButtonWidget(middle, height, 150, 20, ModTexts.NEXT, (buttonWidget) -> {
+            this.client.setScreen(new SpeedrunnerBulkScreen(this.parent, MinecraftClient.getInstance().options));
+        }));
+
+        height += 24;
+        this.addDrawableChild(new ButtonWidget(middle, height, 150, 20, ModTexts.PREVIOUS, (buttonWidget) -> {
+            this.client.setScreen(new EyeOfAnnulScreen(this.parent, MinecraftClient.getInstance().options));
+        }));
+
+        height += 24;
+        this.addDrawableChild(new ButtonWidget(middle, height, 150, 20, ScreenTexts.DONE, (button) -> {
+            this.close();
+        }));
+    }
+
+    @Override
+    public void close() {
+        this.client.setScreen(new FeaturesScreen(this.parent, MinecraftClient.getInstance().options));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 15, 16777215);
+        drawCenteredText(matrices, this.textRenderer, new TranslatableText("speedrunnermod.features.blocks_and_items.speedrunners_eye.line1"), this.width / 2, SpeedrunnerModClient.DOUBLE_LINED_SCREEN_LINE_1, 16777215);
+        drawCenteredText(matrices, this.textRenderer, new TranslatableText("speedrunnermod.features.blocks_and_items.speedrunners_eye.line2"), this.width / 2, SpeedrunnerModClient.DOUBLE_LINED_SCREEN_LINE_2, 16777215);
+
+        int leftSide = this.width / 2 - 155;
+        int rightSide = leftSide + 160;
+        int farRightSide = rightSide + 273;
+        int height = this.height - 24;
+        drawCenteredText(matrices, this.textRenderer, this.pageNumber, farRightSide, height, 16777215);
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+}
