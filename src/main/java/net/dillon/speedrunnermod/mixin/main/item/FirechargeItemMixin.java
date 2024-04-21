6869a8b503c16ab60fc0fba32b97371061900bce
Dillon8775 <dilloneystrickland12@gmail.com>
2024-04-21 16:58:40 -0400

    fix throwable fireballs eye-height

--- src/main/java/net/dillon/speedrunnermod/mixin/main/item/FirechargeItemMixin.java
+++ src/main/java/net/dillon/speedrunnermod/mixin/main/item/FirechargeItemMixin.java
@@ -34,7 +34,7 @@ public class FirechargeItemMixin extends Item {
                 Vec3d lookVec = player.getRotationVec(1.0F);
                 double speed = 1.5;
                 FireballEntity fireball = new FireballEntity(world, player, lookVec.x * speed, lookVec.y * speed, lookVec.z * speed, options().advanced.throwableFireballsExplosionPower);
-                fireball.updatePosition(player.getX(), player.getEyeY(), player.getZ());
+                fireball.updatePosition(player.getX(), player.getEyeY() - 0.235, player.getZ());
                 fireball.setOwner(player);
                 world.spawnEntity(fireball);
                 world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 1.0F, 1.0F);
