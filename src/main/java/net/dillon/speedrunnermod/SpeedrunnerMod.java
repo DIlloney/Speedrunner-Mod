82783526f9053d094b4933c35e377daa01891e98
Dillon8775 <dilloneystrickland12@gmail.com>
2024-04-23 16:44:31 -0400

    1.9.7.1

--- src/main/java/net/dillon/speedrunnermod/SpeedrunnerMod.java
+++ src/main/java/net/dillon/speedrunnermod/SpeedrunnerMod.java
@@ -36,7 +36,7 @@ import static net.dillon.speedrunnermod.option.ModOptions.createListOption;
  */
 public class SpeedrunnerMod implements ModInitializer {
     public static final String MOD_ID = "speedrunnermod";
-    public static final String MOD_VERSION = "v1.9.8";
+    public static final String MOD_VERSION = "v1.9.7.1";
     public static final String VERSION = "Version: " + MOD_VERSION;
     public static final String THE_SPEEDRUNNER_MOD_STRING = "The Speedrunner Mod";
     public static final String WIKI_LINK = "https://sites.google.com/view/dillon8775/the-speedrunner-mod";
