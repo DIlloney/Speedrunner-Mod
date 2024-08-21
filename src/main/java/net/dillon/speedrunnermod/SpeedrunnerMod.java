da1449b0f42754d05565bef76085bfc278307f40
Dillon8775 <dilloneystrickland12@gmail.com>
2024-08-20 22:04:32 -0400

    testing branch!

--- src/main/java/net/dillon/speedrunnermod/SpeedrunnerMod.java
+++ src/main/java/net/dillon/speedrunnermod/SpeedrunnerMod.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ModInitializer;
  * The home initializer for the Speedrunner Mod.
  */
 public class SpeedrunnerMod implements ModInitializer {
+    public static final boolean DOOM_MODE = false;
 
     @Override
     public void onInitialize() {
