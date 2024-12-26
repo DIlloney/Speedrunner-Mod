bc1682286cd6fa6d944771a4210e1f3c025b6258
Dillon8775 <dilloneystrickland12@gmail.com>
2024-12-25 23:48:07 -0500

    bug fix

--- src/main/java/net/dillon/speedrunnermod/mixin/main/fix/PumpkinBlockMixin.java
+++ src/main/java/net/dillon/speedrunnermod/mixin/main/fix/PumpkinBlockMixin.java
@@ -2,18 +2,17 @@ package net.dillon.speedrunnermod.mixin.main.fix;
 
 import net.dillon.speedrunnermod.tag.ModItemTags;
 import net.minecraft.block.PumpkinBlock;
-import net.minecraft.block.TntBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin({PumpkinBlock.class, TntBlock.class})
-public class BlockFixMixin {
+@Mixin(PumpkinBlock.class)
+public class PumpkinBlockMixin {
 
     /**
-     * Fixes {@code speedrunner shears} not working on certain blocks.
+     * Fixes {@code speedrunner shears} not working on pumpkin blocks.
      */
     @Redirect(method = "onUseWithItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
     private boolean onUse(ItemStack stack, Item item) {
---
+++ src/main/java/net/dillon/speedrunnermod/mixin/main/fix/TntBlockMixin.java
@@ -0,0 +1,21 @@
+package net.dillon.speedrunnermod.mixin.main.fix;
+
+import net.dillon.speedrunnermod.tag.ModItemTags;
+import net.minecraft.block.TntBlock;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TntBlock.class)
+public class TntBlockMixin {
+
+    /**
+     * Fixes {@code ignitables} not working on TNT blocks.
+     */
+    @Redirect(method = "onUseWithItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+    private boolean onUse(ItemStack stack, Item item) {
+        return stack.isIn(ModItemTags.IGNITABLES);
+    }
+}
